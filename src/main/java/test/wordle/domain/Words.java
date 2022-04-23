36512345d4caa2bf3fb4846e94fbbb01944f2a8a
MangKyu <whalsrb1226@naver.com>
2022-04-23 15:16:46 +0900

    feature: 5개의 Word가 아닌 경우 IllegalArgumentException 에러를 더진다.

--- src/main/java/test/wordle/domain/Words.java
+++ src/main/java/test/wordle/domain/Words.java
@@ -8,6 +8,9 @@ public class Words {
     private final List<Word> wordList;
 
     public Words(final String text) {
+        if (text.length() != 5) {
+            throw new IllegalArgumentException("length of the word must be 5");
+        }
         wordList = new ArrayList<>();
         for (final char input : text.toCharArray()) {
             wordList.add(new Word(input));
--- src/test/java/test/wordle/domain/WordsTest.java
+++ src/test/java/test/wordle/domain/WordsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WordsTest {
 
@@ -13,6 +14,13 @@ class WordsTest {
         final Words words = new Words(input);
 
         assertThat(words.length()).isEqualTo(5);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abce", "defghs", "def5s"})
+    void Words생성실패(final String input) {
+        assertThatThrownBy(() -> new Words(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
