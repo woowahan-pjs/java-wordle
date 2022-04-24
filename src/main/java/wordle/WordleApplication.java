ce3154d92a09b6b8cbf742c6cb141986efe4a410
minsoozz <minsoo1604@naver.com>
2022-04-24 21:18:34 +0900

    feat: Application main() 추가

---
+++ src/main/java/wordle/WordleApplication.java
@@ -0,0 +1,22 @@
+package wordle;
+
+public class WordleApplication {
+
+    public static void main(String[] args) {
+//        GameManager gameManager;
+//       Input input;
+//        Output output;
+//        Judgement judgement;
+//        CreateAnswer createAnswer;
+
+        /*
+            gameManager.start();
+            createAnswer.execute();
+            for(int i=0;i<6;++i) {
+                input();
+                Result result = judgement.execute();
+                output(result)
+            }
+         */
+    }
+}
