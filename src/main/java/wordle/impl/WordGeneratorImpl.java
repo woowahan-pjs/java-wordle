fd8b5bf0dbf1f96990423ba13c13873a33863d79
minsoozz <minsoo1604@naver.com>
2022-04-24 21:16:48 +0900

    feat: words.txt 파일을 읽어 단어를 반환하는 기능 추가

--- src/main/java/wordle/impl/WordGeneratorImpl.java
+++ src/main/java/wordle/impl/WordGeneratorImpl.java
@@ -1,11 +1,33 @@
 package wordle.impl;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import wordle.design.WordGenerator;
 
 public class WordGeneratorImpl implements WordGenerator {
 
+    // words.txt 파일을 배열로 만들어서 ((현재 날짜 - 2021년 6월 19일) % 배열의 크기) 번째의 단어를 반환한다
+
+    private static final List<String> WORDS = readFile();
+    private static final String REFERENCE_DATE = "2021-06-19";
+
     @Override
     public String execute() {
-        return "maven";
+        return WORDS.get(findIndex());
+    }
+
+    private static List<String> readFile() {
+        return List.of("apple", "adult", "judge", "rower", "artsy", "rural", "shave");
+    }
+
+    private int findIndex() {
+        LocalDate now = LocalDate.now();
+        LocalDate referenceDay = LocalDate.parse(REFERENCE_DATE, DateTimeFormatter.ISO_LOCAL_DATE);
+        Duration diff = Duration.between(now.atStartOfDay(), referenceDay.atStartOfDay());
+        long diffDays = diff.toDays();
+
+        return (int) (diffDays % WORDS.size());
     }
 }
