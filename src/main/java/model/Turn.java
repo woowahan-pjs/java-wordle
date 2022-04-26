645331df4494ab23e3aa3ff60174bd42d82b57e4
Oh yong Soo <dhdydtn2019@gmail.com>
2022-04-27 00:06:13 +0900

    feat(Turn): 한 턴을 진행 했을 때 횟수 증가 기능 추가

---
+++ src/main/java/model/Turn.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Turn {
+
+    private static final int INIT_VALUE = 1;
+    private static final int INCREASE_VALUE = 1;
+
+    private int turn;
+
+    public Turn() {
+        this.turn = INIT_VALUE;
+    }
+
+    public int increase() {
+        return turn + INCREASE_VALUE;
+    }
+}
