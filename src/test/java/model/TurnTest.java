5367b25a3a5bb5fa57fb908e7a0ab78cb7330ddf
Oh yong Soo <dhdydtn2019@gmail.com>
2022-04-27 00:44:43 +0900

    test(Turn): 게임 턴이 종료됬는지 확인 테스트 케이스 추가

--- src/test/java/model/TurnTest.java
+++ src/test/java/model/TurnTest.java
@@ -2,10 +2,11 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TurnTest {
 
@@ -24,9 +25,18 @@ class TurnTest {
     void TurnException() {
         Turn turn = new Turn(6);
 
-        assertThatThrownBy(() -> {
-            turn.increase();
-        }).isInstanceOf(IllegalStateException.class)
+        assertThatThrownBy(() -> turn.increase())
+                .isInstanceOf(IllegalStateException.class)
                 .hasMessage("6번까지 입력 가능합니다.");
+    }
+
+    @ParameterizedTest(name = "턴이 {0}이면 게임 종료 결과는 {1}")
+    @CsvSource({"5,false", "6,true"})
+    void isGameOver(int value, boolean result) {
+        Turn turn = new Turn(value);
+
+        boolean isGameOver = turn.isGameOver();
+
+        assertThat(isGameOver).isEqualTo(result);
     }
 }
