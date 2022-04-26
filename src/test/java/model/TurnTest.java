61627bea1217a8a376dcd380cb6eeefc719659ad
Oh yong Soo <dhdydtn2019@gmail.com>
2022-04-27 00:07:27 +0900

    test(Turn): 테스트 케이스 추가 턴이 6회 초과 증가시 예외발생

--- src/test/java/model/TurnTest.java
+++ src/test/java/model/TurnTest.java
@@ -1,6 +1,8 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,4 +19,14 @@ class TurnTest {
         assertThat(result).isEqualTo(2);
     }
 
+    @DisplayName("턴이 6회 초과 증가시 예외발생")
+    @Test
+    void TurnException() {
+        Turn turn = new Turn(6);
+
+        assertThatThrownBy(() -> {
+            turn.increase();
+        }).isInstanceOf(IllegalStateException.class)
+                .hasMessage("6번까지 입력 가능합니다.");
+    }
 }
