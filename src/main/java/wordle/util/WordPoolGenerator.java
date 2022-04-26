c8f20599b932eab30d065c5c5782229480b374f9
david <xodnjs2549@naver.com>
2022-04-26 23:14:09 +0900

    refactor(WordPoolGenerator): WordsGenerator 이름을 WordPoolGenerator 로 변경

--- src/main/java/wordle/util/WordPoolGenerator.java
+++ src/main/java/wordle/util/WordPoolGenerator.java
@@ -4,15 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import wordle.model.WordPool;
 
-public class WordsGenerator {
+public class WordPoolGenerator {
 
 	public static final String WORDS_TEXT_FILE_PATH = "src/main/resources/words.txt";
 	public static final String NO_SUCH_FILE_EXCEPTION_MESSAGE = "존재하지 않는 파일입니다.";
 
-	public static List<String> generate() {
+	public static WordPool generate() {
 		try {
-			return Files.readAllLines(Paths.get(WORDS_TEXT_FILE_PATH));
+			return new WordPool(Files.readAllLines(Paths.get(WORDS_TEXT_FILE_PATH)));
 		} catch (IOException e) {
 			throw new IllegalArgumentException(NO_SUCH_FILE_EXCEPTION_MESSAGE);
 		}
