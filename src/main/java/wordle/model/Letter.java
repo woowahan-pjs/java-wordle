f70d040f8add3a675dfa97a90dac5ad71dd74035
david <xodnjs2549@naver.com>
2022-04-26 23:17:56 +0900

    feat(Letter): 단어를 구성하는 알파벳과 그 알파벳의 위치를 가지고 있는 객체 추가

---
+++ src/main/java/wordle/model/Letter.java
@@ -0,0 +1,44 @@
+package wordle.model;
+
+import java.util.List;
+
+public class Letter {
+
+	public static final int MINIMUM_POSITION = 1;
+	public static final int MAXIMUM_POSITION = 5;
+	public static final String INVALID_ALPHABET_MESSAGE = "유효하지 않은 단어입니다.";
+	public static final String INVALID_POSITION_MESSAGE = "유효하지 않은 위치입니다.";
+
+	private char alphabet;
+	private int position;
+
+	public Letter(char alphabet, int position) {
+		init(alphabet, position);
+	}
+
+	private void init(char alphabet, int position) {
+		isAlphabet(alphabet);
+		this.alphabet = Character.toLowerCase(alphabet);
+		isValidRange(position);
+		this.position = position;
+	}
+
+	// todo
+	public TileStatus compareResult(List<Letter> letters) {
+		return TileStatus.GRAY;
+	}
+
+	private void isAlphabet(char alphabet) {
+		if(Character.isAlphabetic(alphabet)) {
+			return;
+		}
+		throw new IllegalArgumentException(INVALID_ALPHABET_MESSAGE);
+	}
+
+	private void isValidRange(int position) {
+		if (MINIMUM_POSITION <= position && position <= MAXIMUM_POSITION) {
+			return;
+		}
+		throw new IllegalArgumentException(INVALID_POSITION_MESSAGE);
+	}
+}
