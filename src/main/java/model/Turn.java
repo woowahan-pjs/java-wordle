28f77c332d007b7aa39f7f5ed7635f1ef71d967a
Oh yong Soo <dhdydtn2019@gmail.com>
2022-04-27 00:45:04 +0900

    feat(Turn): 게임 턴이 종료됬는지 확인 기능 추가

--- src/main/java/model/Turn.java
+++ src/main/java/model/Turn.java
@@ -4,6 +4,8 @@ public class Turn {
 
     private static final int INIT_VALUE = 1;
     private static final int INCREASE_VALUE = 1;
+    private static final int BOUNDARY_VALUE = 6;
+    private static final String TURN_BOUNDARY_OVER_ERR_MSG = "6번까지 입력 가능합니다.";
 
     private int turn;
 
@@ -11,7 +13,26 @@ public class Turn {
         this(INIT_VALUE);
     }
 
+    Turn(int turn) {
+        this.turn = turn;
+    }
+
     public int increase() {
-        return turn + INCREASE_VALUE;
+        validateIncrease();
+        return turn += INCREASE_VALUE;
+    }
+
+    private void validateIncrease() {
+        if (turn + INCREASE_VALUE > BOUNDARY_VALUE) {
+            throw new IllegalStateException(TURN_BOUNDARY_OVER_ERR_MSG);
+        }
+    }
+
+    public boolean isGameOver() {
+        if(turn == BOUNDARY_VALUE) {
+            return true;
+        }
+
+        return false;
     }
 }
