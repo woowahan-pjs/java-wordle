8a9786b32c6cd00c88b0833d56ef1acf9e9dc234
Oh yong Soo <dhdydtn2019@gmail.com>
2022-04-30 22:16:25 +0900

    fix: 안올라간 파일 재 푸시

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import controller.WordleGame;
+
+public class Application {
+
+    public static void main(String[] args) {
+        WordleGame wordleGame = new WordleGame();
+        wordleGame.play();
+    }
+}
