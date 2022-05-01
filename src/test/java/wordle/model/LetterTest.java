2fb873b4bb464568c46286ae6804c7bc296e078a
david <xodnjs2549@naver.com>
2022-05-01 18:12:44 +0900

    test(LetterTest): Letter 내 Position 객체 삭제로 인한 변경

--- src/test/java/wordle/model/LetterTest.java
+++ src/test/java/wordle/model/LetterTest.java
@@ -4,27 +4,17 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LetterTest {
 
 	@ParameterizedTest
-	@CsvSource(value = {"ㅁ:1", "&:1"}, delimiter = ':')
+	@ValueSource(strings = {"ㅁ", "&"})
 	@DisplayName("Letter 객체 생성시 알파벳이 아닌 값을 입력하면 오류가 발생한다")
-	void invalidAlphabetInputTest(char alphabet, int position) {
+	void invalidAlphabetInputTest(char alphabet) {
 		// given & when & then
 		assertThatIllegalArgumentException().isThrownBy(
-			() -> new Letter(alphabet, position)
-		);
-	}
-
-	@ParameterizedTest
-	@CsvSource(value = {"a:0", "a:6", "a:-1"}, delimiter = ':')
-	@DisplayName("Letter 객체 생성시 유효하지 않은 범위의 값을 입력하면 오류가 발생한다")
-	void invalidPositionInputTest(char alphabet, int position) {
-		// given & when & then
-		assertThatIllegalArgumentException().isThrownBy(
-			() -> new Letter(alphabet, position)
+			() -> new Letter(alphabet)
 		);
 	}
 }
