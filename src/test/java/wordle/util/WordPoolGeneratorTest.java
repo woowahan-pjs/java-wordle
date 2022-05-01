2b8ddccbf9343e7642c46a23bf73a8ed6951b7e6
david <xodnjs2549@naver.com>
2022-05-01 23:48:43 +0900

    test(WordPoolGeneratorTest): 테스트 추가
    - 파일 읽기, 없는 파일 경로 읽기, 5글자가 아닌 단어 읽기에 대한 테스트

--- src/test/java/wordle/util/WordPoolGeneratorTest.java
+++ src/test/java/wordle/util/WordPoolGeneratorTest.java
@@ -1,6 +1,7 @@
 package wordle.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +11,22 @@ class WordPoolGeneratorTest {
 
 	private static final String VALID_WORDS_TEXT_FILE_PATH = "src/test/resources/words.txt";
 	private static final String INVALID_WORDS_TEXT_FILE_PATH = "src/test/resources/invalidWords.txt";
+	private static final String NOT_EXISTED_TEXT_FILE_PATH = "src/test/resources/notExistedFile.txt";
 
 	@Test
-	@DisplayName("words.txt 파일의 추출로 가져온 단어의 갯수는 0보다 크다")
-	void wordsGeneratorTest() {
+	@DisplayName("기본 파일 경로로부터 단어 목록이 생성된다")
+	void wordsGenerateWithInternalDefaultFilePath() {
+		// given & when
+		WordPool wordList = WordPoolGenerator.generate();
+		int wordListSize = wordList.size();
+
+		// then
+		assertThat(wordListSize).isPositive();
+	}
+
+	@Test
+	@DisplayName("외부 파일 경로로부터 단어 목록이 생성된다")
+	void wordsGenerateWithExternalFilePath() {
 		// given & when
 		WordPool wordList = WordPoolGenerator.generate(VALID_WORDS_TEXT_FILE_PATH);
 		int wordListSize = wordList.size();
@@ -23,17 +36,18 @@ class WordPoolGeneratorTest {
 	}
 
 	@Test
-	@DisplayName("invalidWords.txt 파일의 추출로 가져온 단어의 갯수는 0이다")
-	void validWordLengthTest() {
-		// 유효하지 않은 길이의 단어를 가지고 있는 파일을 검증하기 위해서 어떤 방법이 좋은지?
-		// 1. 미리 만들어진 테스트용 파일(유효하지 않은 단어 목록)을 사용한다.
-		// 2. 테스트 내에서 테스트용 파일(유효하지 않은 단어 목록)을 만들어 사용한다.
-		// 3. 테스트 내에서 이미 만들어진 테스트용 파일을 읽어 온 결과와 객체(WordPoolGenerator)로부터 만들어진 결과를 비교한다.
-		// given & when
-		WordPool wordList = WordPoolGenerator.generate(INVALID_WORDS_TEXT_FILE_PATH);
-		int wordListSize = wordList.size();
+	@DisplayName("존재하지 않는 파일의 경로로 단어 목록을 생성하려고 하면 예외가 발생한다")
+	void throwExceptionFromNotExistedFilePath() {
+		// given & when & then
+		assertThatIllegalArgumentException().
+			isThrownBy(() -> WordPoolGenerator.generate(NOT_EXISTED_TEXT_FILE_PATH));
+	}
 
-		// then
-		assertThat(wordListSize).isZero();
+	@Test
+	@DisplayName("5글자로 구성된 단어가 1개라도 존재하지 않으면 예외가 발생한다")
+	void throwExceptionFromNotExistedValidLengthWords() {
+		// given & when & then
+		assertThatIllegalArgumentException().
+			isThrownBy(() -> WordPoolGenerator.generate(INVALID_WORDS_TEXT_FILE_PATH));
 	}
 }
