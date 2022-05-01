45c1c20666d22e753d79f958ae66b24a3b3182c4
david <xodnjs2549@naver.com>
2022-05-01 17:55:08 +0900

    feat(Game): 단어를 비교하여 일치 여부를 저장하는 기능 추가

--- src/main/java/wordle/model/Game.java
+++ src/main/java/wordle/model/Game.java
@@ -2,22 +2,57 @@ package wordle.model;
 
 public class Game {
 
-	// grid
-	// words
-	// gameStatus
+	private static final int MAX_TRYING_COUNT = 6;
 	private Word answer;
+	private int tryingCount;
+	private Grid grid = new Grid();
+	private boolean isFinish = false;
 
-	// Refactor: wordList를 계속 넘기는 것
 	public Game(WordPool wordList) {
 		init(wordList);
 	}
 
 	private void init(WordPool wordList) {
-		//TODO: 오늘 답안 생성
 		answer = new Word(wordList.getTodayAnswerWord());
 	}
 
 	public boolean isFinish() {
-		return false;
+		return isFinish;
+	}
+
+	public void compareWith(String userInput) {
+		// 오늘 정답 단어와 입력받은 단어와 비교 후 결과 출력
+		Word userInputWord = new Word(userInput);
+		Tiles tiles = answer.calculateMatched(userInputWord);
+		grid.addTiles(tiles);
+		plusTryingCount();
+		if (allMatched(tiles)) {
+			grid.addAnswerWord(answer);
+			setFinish();
+			grid.giveAnswer();
+			return;
+		}
+		if (tryingCount == MAX_TRYING_COUNT) {
+			setFinish();
+			grid.addAnswerWord(answer);
+			return;
+		}
+		answer.clearAllMatched();
+	}
+
+	private boolean allMatched(Tiles tiles) {
+		return tiles.isAllMatched();
+	}
+
+	private void setFinish() {
+		this.isFinish = true;
+	}
+
+	private void plusTryingCount() {
+		tryingCount++;
+	}
+
+	public Grid getResult() {
+		return grid;
 	}
 }
