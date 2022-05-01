8318ffc407f4c0958850723c9e0dafab0a508530
david <xodnjs2549@naver.com>
2022-05-01 17:46:51 +0900

    feat(TileStatus): TileStatus 내 이모지 정보 추가

--- src/main/java/wordle/model/TileStatus.java
+++ src/main/java/wordle/model/TileStatus.java
@@ -2,5 +2,15 @@ package wordle.model;
 
 public enum TileStatus {
 
-	GREEN, YELLOW, GRAY
+	GREEN("\uD83D\uDFE9"), YELLOW("\uD83D\uDFE8"), GRAY("⬜");
+
+	private final String unicode;
+
+	TileStatus(String unicode) {
+		this.unicode = unicode;
+	}
+
+	public String getUnicode() {
+		return this.unicode;
+	}
 }
