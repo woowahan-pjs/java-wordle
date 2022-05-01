127f51e42c5f3c8baf19c13a2b744be186b391a3
flamme <flamme1004@gmail.com>
2022-05-01 17:38:03 +0900

    feat(Answer.class) : 정답 글자도 안맞고, 위치도 다른 경우는 그레이이다

--- src/test/java/domain/AnswerTest.java
+++ src/test/java/domain/AnswerTest.java
@@ -20,7 +20,7 @@ public class AnswerTest {
         assertThat(answer.compare(userAnswer).getList()).containsOnly(LetterResult.GREEN);
     }
 
-    @DisplayName(" 정답과 답안이 모두 맞는 경우에는 모두 그린이다")
+    @DisplayName(" 정답과 답안이 모두 맞는 경우에는 모두 GREEN 이다")
     @Test
     void compare_5green() {
         Letters todayAnswer = Letters.of("abcde");
@@ -30,7 +30,7 @@ public class AnswerTest {
         assertThat(answer.compare(userAnswer).getList()).containsOnly(LetterResult.GREEN);
     }
 
-    @DisplayName(" 정답과 답안를 비교하여 단어와 위치가 일치하면 그린이다")
+    @DisplayName(" 정답과 답안를 비교하여 단어와 위치가 일치하면 GREEN 이다")
     @ParameterizedTest
     @CsvSource({"hello, 5", "aallo, 3", "hasde, 1"})
     void compare_green(String input, int expected) {
@@ -58,6 +58,21 @@ public class AnswerTest {
                 answer.compare(userAnswer)
                         .getList()
                         .stream().filter(letterResult -> letterResult == LetterResult.YELLOW).collect(Collectors.toList())
+        ).hasSize(expected);
+    }
+
+    @DisplayName(" 정답과 답안를 비교하여 정답 글자도 안맞고, 위치도 다른 경우 GRAY 이다")
+    @ParameterizedTest
+    @CsvSource({"asbcd, 5", "aheob, 2"})
+    void compare_gray(String input, int expected) {
+        Letters todayAnswer = Letters.of("hello");
+        Answer answer = new Answer(todayAnswer);
+        Letters userAnswer = Letters.of(input);
+
+        assertThat(
+                answer.compare(userAnswer)
+                        .getList()
+                        .stream().filter(letterResult -> letterResult == LetterResult.GRAY).collect(Collectors.toList())
         ).hasSize(expected);
     }
 }
