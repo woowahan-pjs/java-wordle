82809265c64af3506526d6dbcd9bf6fbf4876913
david <xodnjs2549@naver.com>
2022-05-01 23:28:24 +0900

    refactor(WordPoolGenerator): 파일 읽기, 단어 검증, 단어목록 크기 검증 메서드 분리

--- src/main/java/wordle/util/WordPoolGenerator.java
+++ src/main/java/wordle/util/WordPoolGenerator.java
@@ -1,38 +1,50 @@
 package wordle.util;
 
-import static java.nio.file.Files.newBufferedReader;
-
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import wordle.model.WordPool;
 
 public class WordPoolGenerator {
 
-	public static final String WORDS_TEXT_FILE_PATH = "src/main/resources/words.txt";
-	private static final String NO_SUCH_FILE_EXCEPTION_MESSAGE = "존재하지 않는 파일입니다.";
+	private static final String DEFAULT_WORDS_TEXT_FILE_PATH = "src/main/resources/words.txt";
 	private static final int VALID_WORD_LENGTH = 5;
+	private static final int INVALID_WORDS_SIZE = 0;
+	private static final String NO_SUCH_FILE_EXCEPTION_MESSAGE = "존재하지 않는 파일입니다.";
+	private static final String NOT_EXISTED_WORD_IN_FILE_MESSAGE = "파일에 단어가 존재하지 않습니다.";
+
+	public static WordPool generate() {
+		return generate(DEFAULT_WORDS_TEXT_FILE_PATH);
+	}
 
 	public static WordPool generate(String path) {
-		List<String> words = new ArrayList<>();
-		String word = null;
-
-		// todo:refactor: depth 줄이기
-		try (BufferedReader reader = newBufferedReader(Paths.get(path))) {
-			do {
-				word = reader.readLine();
-				if (word != null && word.length() == VALID_WORD_LENGTH) {
-					words.add(word);
-				}
-			} while (word != null);
-		} catch (IOException e) {
-			throw new IllegalArgumentException(NO_SUCH_FILE_EXCEPTION_MESSAGE);
-		}
-
+		List<String> words;
+		words = readWordsFromFile(path);
+		words = filterWordAsFiveLetters(words);
+		validateWordsSize(words);
 		return new WordPool(words);
 	}
 
-	// todo 파일에 단어가 1개이상 존재해야 한다. 단어가 0개면 예외처리
+	private static List<String> filterWordAsFiveLetters(List<String> words) {
+		return words.stream().filter(word -> word.length() == VALID_WORD_LENGTH)
+			.collect(Collectors.toList());
+	}
+
+	private static List<String> readWordsFromFile(String path) {
+		List<String> words;
+		try {
+			words = Files.readAllLines(Paths.get(path));
+		} catch (IOException e) {
+			throw new IllegalArgumentException(NO_SUCH_FILE_EXCEPTION_MESSAGE);
+		}
+		return words;
+	}
+
+	private static void validateWordsSize(List<String> words) {
+		if (words.size() == INVALID_WORDS_SIZE) {
+			throw new IllegalArgumentException(NOT_EXISTED_WORD_IN_FILE_MESSAGE);
+		}
+	}
 }
