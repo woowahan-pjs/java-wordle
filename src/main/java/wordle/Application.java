8f31904513253823091769cf56273eff94003fbb
david <xodnjs2549@naver.com>
2022-05-01 18:16:12 +0900

    feat(Application): 프로그램 시작점 추가

--- src/main/java/wordle/Application.java
+++ src/main/java/wordle/Application.java
@@ -1,7 +1,10 @@
 package wordle;
 
+import wordle.controller.WordleMachine;
+
 public class Application {
 	public static void main(String[] args) {
-		// TODO: 프로그램 구현
+		WordleMachine wordleMachine = new WordleMachine();
+		wordleMachine.startGame();
 	}
 }
