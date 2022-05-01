b07ef9114d66f9fcad56f82c86fa78246c41fc0d
david <xodnjs2549@naver.com>
2022-05-01 23:59:04 +0900

    refactor(Tiles): 불필요한 메서드 삭제

--- src/main/java/wordle/model/Tiles.java
+++ src/main/java/wordle/model/Tiles.java
@@ -2,7 +2,6 @@ package wordle.model;
 
 public class Tiles {
 
-	private static final int MAX_TILE_STATUS_COUNT = 5;
 	private TileStatus[] status;
 
 	public Tiles(TileStatus[] status) {
@@ -11,20 +10,5 @@ public class Tiles {
 
 	public TileStatus[] getStatus() {
 		return status;
-	}
-
-	public boolean isAllMatched() {
-		int greenCount = 0;
-
-		for (TileStatus tileStatus : status) {
-			if (tileStatus == TileStatus.GREEN) {
-				greenCount++;
-			}
-		}
-
-		if (greenCount == MAX_TILE_STATUS_COUNT) {
-			return true;
-		}
-		return false;
 	}
 }
