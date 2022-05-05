5c85687247589076d61265a0a8ad48079089ee94
jaenyeong <jaenyeong.dev@gmail.com>
2022-05-05 20:21:30 +0900

    refactor(WordFile at el): Inline `File`, `Scanner` field to method and refactor test method

--- src/main/java/wordle/domain/WordFile.java
+++ src/main/java/wordle/domain/WordFile.java
@@ -2,26 +2,25 @@ package wordle.domain;
 
 import java.io.File;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 public class WordFile {
     private static final String FILE_PATH = "./src/main/resources/";
-    private final File wordFile;
-    private final Scanner scanner;
     private final List<String> wordList = new ArrayList<>();
 
     public WordFile(String fileName) {
-        wordFile = new File(FILE_PATH + fileName);
+        final File wordFile = new File(FILE_PATH + fileName);
 
         if (!wordFile.exists()) {
             throw new IllegalArgumentException("올바른 파일이 필요합니다.");
         }
 
         try {
-            scanner = new Scanner(wordFile);
+            final Scanner scanner = new Scanner(wordFile);
 
             while (scanner.hasNext()) {
                 wordList.add(scanner.nextLine());
@@ -34,7 +33,7 @@ public class WordFile {
     public String findTargetWord(LocalDate targetDate) {
         final LocalDate conditionDate = LocalDate.of(2021, 6, 19);
 
-        final long diffDays = ChronoUnit.DAYS.between(conditionDate, targetDate);
+        final long diffDays = DAYS.between(conditionDate, targetDate);
 
         final int targetIdx = Math.abs((int) diffDays % wordList.size());
 
