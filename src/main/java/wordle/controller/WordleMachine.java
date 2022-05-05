20ec5c0c02867b60fa129ae0666217bb2ce8226d
Celina <luv.mins99@gmail.com>
2022-05-06 00:47:08 +0900

    refactor(WordleMachine): WordPool 생성 로직 수정, 게임 진행 상태 관련 로직 수정

--- src/main/java/wordle/controller/WordleMachine.java
+++ src/main/java/wordle/controller/WordleMachine.java
@@ -1,6 +1,7 @@
 package wordle.controller;
 
 import wordle.model.Game;
+import wordle.model.GameStatus;
 import wordle.model.WordPool;
 import wordle.util.WordPoolGenerator;
 import wordle.util.WordValidator;
@@ -9,21 +10,21 @@ import wordle.view.ConsoleOutput;
 
 public class WordleMachine {
 
-	private static final WordPool WORD_POOL = WordPoolGenerator.generate();
+	private static final WordPool WORD_POOL = new WordPool(WordPoolGenerator.generate());
 	private final ConsoleInput consoleInput = new ConsoleInput();
 	private final ConsoleOutput consoleOutput = new ConsoleOutput();
 
 	public void startGame() {
 		Game game = new Game(WORD_POOL);
 		consoleOutput.printGameStartMessage();
-		while (!game.isFinish()) {
+		while (game.getGameStatus() == GameStatus.PROGRESS) {
 			game.compareWith(readValidUserInput());
 			consoleOutput.printGameResultMessage(game.getResult(), game.getAnswer());
 		}
 	}
 
 	private String readValidUserInput() {
-		String userInputWord = "";
+		String userInputWord;
 
 		do {
 			userInputWord = consoleInput.readUserInput();
