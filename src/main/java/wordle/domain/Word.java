3e2ad6f8d5b3941dbb61ae4d54170b30b495c9cc
jaenyeong <jaenyeong.dev@gmail.com>
2022-05-06 00:48:29 +0900

    feat(Word at el): Add `compare` feature in `Word` class

--- src/main/java/wordle/domain/Word.java
+++ src/main/java/wordle/domain/Word.java
@@ -2,6 +2,13 @@ package wordle.domain;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static wordle.domain.BingoStatus.CONTAIN;
+import static wordle.domain.BingoStatus.MATCH;
+import static wordle.domain.BingoStatus.NOTHING;
+
 public class Word {
     private static final int WORD_LENGTH = 5;
     private final String word;
@@ -16,6 +23,41 @@ public class Word {
 
     private boolean isNotValid(String word) {
         return StringUtils.isBlank(word) || word.length() != WORD_LENGTH;
+    }
+
+    public List<BingoStatus> compare(Word otherWord) {
+        final List<BingoStatus> resultBingoStatus = new ArrayList<>();
+
+        for (int i = 0; i < WORD_LENGTH; i++) {
+            final char otherChar = otherWord.word.charAt(i);
+            final char myChar = word.charAt(i);
+
+            bindingStatusByBingo(resultBingoStatus, myChar, otherChar);
+        }
+
+        return resultBingoStatus;
+    }
+
+    private void bindingStatusByBingo(List<BingoStatus> resultBingoStatus, char myChar, char otherChar) {
+        if (isExactlyMatch(myChar, otherChar)) {
+            resultBingoStatus.add(MATCH);
+            return;
+        }
+
+        if (isContains(otherChar)) {
+            resultBingoStatus.add(CONTAIN);
+            return;
+        }
+
+        resultBingoStatus.add(NOTHING);
+    }
+
+    private Boolean isExactlyMatch(char myChar, char otherChar) {
+        return myChar == otherChar;
+    }
+
+    private Boolean isContains(char otherChar) {
+        return word.contains(String.valueOf(otherChar));
     }
 
     public String getWord() {
--- src/test/java/wordle/domain/WordTest.java
+++ src/test/java/wordle/domain/WordTest.java
@@ -2,11 +2,17 @@ package wordle.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static wordle.domain.BingoStatus.*;
 
 class WordTest {
 
@@ -46,5 +52,30 @@ class WordTest {
             new Word(givenWord)
         ).isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("유효하지 않은 단어 문자열 입니다.");
+    }
+
+    @DisplayName("주어진 `Word`가 타겟 `Word`와 일치하는 지 비교하면 비교 결과를 리스트에 담아 반환")
+    @ParameterizedTest
+    @MethodSource("givenWordsAndTargetWordsAndExpectedBingoStatusList")
+    void returnBingoStatusListWhenCompareBetweenGivenWordAndTargetWord(Word targetWord, Word givenWord, List<BingoStatus> expectedResult) {
+        // Arrange
+        // Act
+        final List<BingoStatus> result = targetWord.compare(givenWord);
+
+        // Assert
+        assertThat(result.size()).isEqualTo(targetWord.getWord().length());
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> givenWordsAndTargetWordsAndExpectedBingoStatusList() {
+        return Stream.of(
+            Arguments.of(new Word("happy"), new Word("apple"), List.of(CONTAIN, CONTAIN, MATCH, NOTHING, NOTHING)),
+            Arguments.of(new Word("heath"), new Word("melon"), List.of(NOTHING, MATCH, NOTHING, NOTHING, NOTHING)),
+            Arguments.of(new Word("gamma"), new Word("trust"), List.of(NOTHING, NOTHING, NOTHING, NOTHING, NOTHING)),
+            Arguments.of(new Word("break"), new Word("bread"), List.of(MATCH, MATCH, MATCH, MATCH, NOTHING)),
+            Arguments.of(new Word("sonic"), new Word("conic"), List.of(CONTAIN, MATCH, MATCH, MATCH, MATCH)),
+            Arguments.of(new Word("track"), new Word("tract"), List.of(MATCH, MATCH, MATCH, MATCH, CONTAIN)),
+            Arguments.of(new Word("state"), new Word("state"), List.of(MATCH, MATCH, MATCH, MATCH, MATCH))
+        );
     }
 }
