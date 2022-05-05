6f2ea12812db9227279d01addbb4d59db0b4add1
Celina <luv.mins99@gmail.com>
2022-05-06 00:20:20 +0900

    refactor(Grid): 게임 결과 객체를 분리
    - Grid가 입력결과 생성된 타일 리스트만 가지도록 변경

--- src/main/java/wordle/model/Grid.java
+++ src/main/java/wordle/model/Grid.java
@@ -4,12 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Grid {
-
-	// todo tilesList -> Grid
-	// todo GameResult: Grid, ...
 	private static final int MAX_TRYING_COUNT = 6;
-	private List<Tiles> tilesList = new ArrayList<>();
-	private boolean isCorrect = false;
+	private final List<Tiles> tilesList = new ArrayList<>();
 
 	public Grid() {
 	}
@@ -22,21 +18,5 @@ public class Grid {
 
 	public List<Tiles> getTilesList() {
 		return tilesList;
-	}
-
-	public int getTryingCount() {
-		return tilesList.size();
-	}
-
-	public boolean isFinishedInTrying() {
-		return isCorrect;
-	}
-
-	public boolean isOverTrying() {
-		return tilesList.size() == MAX_TRYING_COUNT && !isCorrect;
-	}
-
-	public void corrected() {
-		isCorrect = true;
 	}
 }
