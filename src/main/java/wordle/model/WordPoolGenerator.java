2baf689a109077a49a6fd232881ce3d3b27c2c6e
david <xodnjs2549@naver.com>
2022-05-05 15:50:10 +0900

    refactor(WordPool): 메서드명, 변수명, 테스트명 변경

--- src/main/java/wordle/model/WordPoolGenerator.java
+++ src/main/java/wordle/model/WordPoolGenerator.java
@@ -5,21 +5,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import wordle.model.WordPool;
 
 public class WordPoolGenerator {
 
-	private static final String DEFAULT_WORDS_TEXT_FILE_PATH = "src/main/resources/words.txt";
 	private static final int VALID_WORD_LENGTH = 5;
-	private static final int INVALID_WORDS_SIZE = 0;
-	private static final String NO_SUCH_FILE_EXCEPTION_MESSAGE = "존재하지 않는 파일입니다.";
+	private static final int ZERO = 0;
+	private static final String DEFAULT_WORDS_TEXT_FILE_PATH = "src/main/resources/words.txt";
+	private static final String NO_SUCH_FILE_MESSAGE = "존재하지 않는 파일입니다.";
 	private static final String NOT_EXISTED_WORD_IN_FILE_MESSAGE = "파일에 단어가 존재하지 않습니다.";
 
-	public static WordPool generate() {
-		return generate(DEFAULT_WORDS_TEXT_FILE_PATH);
+	public static WordPool generateFromDefaultFile() {
+		return generateFromFile(DEFAULT_WORDS_TEXT_FILE_PATH);
 	}
 
-	public static WordPool generate(String path) {
+	public static WordPool generateFromFile(String path) {
 		List<String> words;
 		words = readWordsFromFile(path);
 		words = filterWordAsFiveLetters(words);
@@ -33,17 +32,15 @@ public class WordPoolGenerator {
 	}
 
 	private static List<String> readWordsFromFile(String path) {
-		List<String> words;
 		try {
-			words = Files.readAllLines(Paths.get(path));
+			return Files.readAllLines(Paths.get(path));
 		} catch (IOException e) {
-			throw new IllegalArgumentException(NO_SUCH_FILE_EXCEPTION_MESSAGE);
+			throw new IllegalArgumentException(NO_SUCH_FILE_MESSAGE);
 		}
-		return words;
 	}
 
 	private static void validateWordsSize(List<String> words) {
-		if (words.size() == INVALID_WORDS_SIZE) {
+		if (words.size() == ZERO) {
 			throw new IllegalArgumentException(NOT_EXISTED_WORD_IN_FILE_MESSAGE);
 		}
 	}
--- src/test/java/wordle/model/WordPoolGeneratorTest.java
+++ src/test/java/wordle/model/WordPoolGeneratorTest.java
@@ -5,8 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wordle.model.WordPool;
-import wordle.model.WordPoolGenerator;
 
 class WordPoolGeneratorTest {
 
@@ -15,40 +13,34 @@ class WordPoolGeneratorTest {
 	private static final String NOT_EXISTED_TEXT_FILE_PATH = "src/test/resources/notExistedFile.txt";
 
 	@Test
-	@DisplayName("기본 파일 경로로부터 단어 목록이 생성된다")
-	void wordsGenerateWithInternalDefaultFilePath() {
-		// given & when
-		WordPool wordList = WordPoolGenerator.generate();
-		int wordListSize = wordList.size();
+	@DisplayName("기본 파일로부터 단어 목록이 생성된다")
+	void words_are_generated_from_default_file() {
+		WordPool wordPool = WordPoolGenerator.generateFromDefaultFile();
+		int wordPoolSize = wordPool.size();
 
-		// then
-		assertThat(wordListSize).isPositive();
+		assertThat(wordPoolSize).isPositive();
 	}
 
 	@Test
-	@DisplayName("외부 파일 경로로부터 단어 목록이 생성된다")
-	void wordsGenerateWithExternalFilePath() {
-		// given & when
-		WordPool wordList = WordPoolGenerator.generate(VALID_WORDS_TEXT_FILE_PATH);
-		int wordListSize = wordList.size();
+	@DisplayName("파일로부터 단어 목록이 생성된다")
+	void words_are_generated_from_file() {
+		WordPool wordPool = WordPoolGenerator.generateFromFile(VALID_WORDS_TEXT_FILE_PATH);
+		int wordPoolSize = wordPool.size();
 
-		// then
-		assertThat(wordListSize).isPositive();
+		assertThat(wordPoolSize).isPositive();
 	}
 
 	@Test
-	@DisplayName("존재하지 않는 파일의 경로로 단어 목록을 생성하려고 하면 예외가 발생한다")
+	@DisplayName("존재하지 않는 파일로 단어 목록을 생성할 때 예외가 발생한다")
 	void throwExceptionFromNotExistedFilePath() {
-		// given & when & then
 		assertThatIllegalArgumentException().
-			isThrownBy(() -> WordPoolGenerator.generate(NOT_EXISTED_TEXT_FILE_PATH));
+			isThrownBy(() -> WordPoolGenerator.generateFromFile(NOT_EXISTED_TEXT_FILE_PATH));
 	}
 
 	@Test
-	@DisplayName("5글자로 구성된 단어가 1개라도 존재하지 않으면 예외가 발생한다")
+	@DisplayName("5글자로 구성된 단어가 1개이상 존재하지 않으면 예외가 발생한다")
 	void throwExceptionFromNotExistedValidLengthWords() {
-		// given & when & then
 		assertThatIllegalArgumentException().
-			isThrownBy(() -> WordPoolGenerator.generate(INVALID_WORDS_TEXT_FILE_PATH));
+			isThrownBy(() -> WordPoolGenerator.generateFromFile(INVALID_WORDS_TEXT_FILE_PATH));
 	}
 }
