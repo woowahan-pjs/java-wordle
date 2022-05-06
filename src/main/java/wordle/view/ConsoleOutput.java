20fddbe9e5fbdc58c34bd363e8eb00470221466e
david <xodnjs2549@naver.com>
2022-05-06 15:48:41 +0900

    feat(ExceptionMessage): 예외 메시지 보관 클래스 추가

--- src/main/java/wordle/view/ConsoleOutput.java
+++ src/main/java/wordle/view/ConsoleOutput.java
@@ -1,5 +1,6 @@
 package wordle.view;
 
+import wordle.model.ExceptionMessage;
 import wordle.model.Grid;
 import wordle.model.TileStatus;
 import wordle.model.Tiles;
@@ -12,11 +13,9 @@ public class ConsoleOutput {
 	private static final int MAX_TRYING_COUNT = 6;
 	private static final String GAME_START_MESSAGE_FORMAT =
 		"WORDLE을 %d번 만에 맞춰 보세요." + NEW_LINE + "시도의 결과는 타일의 색 변화로 나타납니다." + NEW_LINE;
-	private static final String INVALID_WORD_MESSAGE = "존재하지 않는 단어입니다.";
 	private static final String TODAY_ANSWER_MESSAGE = "오늘의 정답 단어는 %s 입니다.";
 	private static final String TRYING_COUNT_MESSAGE_FORMAT = NEW_LINE + "%d" + DIVIDER + MAX_TRYING_COUNT + NEW_LINE;
-	private static final String INVALID_LENGTH_MESSAGE = "단어는 5글자로 구성되어야 합니다.";
-	private static final String INVALID_ALPHABET_MESSAGE = "단어는 영문자로 구성되어야 합니다.";
+
 
 
 	public void printGameStartMessage() {
@@ -41,15 +40,15 @@ public class ConsoleOutput {
 	}
 
 	public static void printInvalidLengthMessage() {
-		System.out.println(INVALID_LENGTH_MESSAGE);
+		System.out.println(ExceptionMessage.INVALID_LENGTH_MESSAGE);
 	}
 
 	public static void printInvalidAlphabetMessage() {
-		System.out.println(INVALID_ALPHABET_MESSAGE);
+		System.out.println(ExceptionMessage.INVALID_ALPHABET_MESSAGE);
 	}
 
 	public static void printInvalidWordMessage() {
-		System.out.println(INVALID_WORD_MESSAGE);
+		System.out.println(ExceptionMessage.INVALID_WORD_MESSAGE);
 	}
 
 	private void printGameResult(Tiles tile) {
