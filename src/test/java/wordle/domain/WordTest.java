530542ada50587dab4c7f6c0a4bea48568181ed7
jaenyeong <jaenyeong.dev@gmail.com>
2022-05-06 10:21:55 +0900

    refactor(WordTest): Update `test code` typo

--- src/test/java/wordle/domain/WordTest.java
+++ src/test/java/wordle/domain/WordTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
@@ -12,7 +12,9 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static wordle.domain.BingoStatus.*;
+import static wordle.domain.BingoStatus.CONTAIN;
+import static wordle.domain.BingoStatus.MATCH;
+import static wordle.domain.BingoStatus.NOTHING;
 
 class WordTest {
 
@@ -28,9 +30,9 @@ class WordTest {
         assertThat(word.getWord()).isEqualTo(givenWord);
     }
 
-    @DisplayName("Null 문자열이 주어진 경우 `Word` 객체 생성 실패")
+    @DisplayName("`Null` 또는 빈 문자열이 주어진 경우 `Word` 객체 생성 실패")
     @ParameterizedTest
-    @NullSource
+    @NullAndEmptySource
     void createWordIsFailIfGivenWordIsNull(String givenWord) {
         // Arrange
         // Act
@@ -43,7 +45,7 @@ class WordTest {
 
     @DisplayName("5글자가 아닌 문자열이 주어진 경우 `Word` 객체 생성 실패")
     @ParameterizedTest
-    @ValueSource(strings = {"", "S", "HA", "sun", "Next", "Kotlin", "happy new year"})
+    @ValueSource(strings = {"   ", "S", "HA", "sun", "Next", "Kotlin", "happy new year"})
     void createWordIsFailIfGivenWordLengthIsNotEqualToFive(String givenWord) {
         // Arrange
         // Act
@@ -54,20 +56,20 @@ class WordTest {
             .hasMessageContaining("유효하지 않은 단어 문자열 입니다.");
     }
 
-    @DisplayName("주어진 `Word`가 타겟 `Word`와 일치하는 지 비교하면 비교 결과를 리스트에 담아 반환")
+    @DisplayName("주어진 `Word`가 정답 `Word`와 일치하는 지 비교하면 비교 결과를 리스트에 담아 반환")
     @ParameterizedTest
-    @MethodSource("givenWordsAndTargetWordsAndExpectedBingoStatusList")
-    void returnBingoStatusListWhenCompareBetweenGivenWordAndTargetWord(Word targetWord, Word givenWord, List<BingoStatus> expectedResult) {
+    @MethodSource("goalWordAndGivenWordAndExpectedBingoStatuses")
+    void returnBingoStatusesWhenCompareBetweenGoalWordAndGivenWord(Word goalWord, Word givenWord, List<BingoStatus> expectedBingoStatuses) {
         // Arrange
         // Act
-        final List<BingoStatus> result = targetWord.compare(givenWord);
+        final List<BingoStatus> resultBingoStatuses = goalWord.compare(givenWord);
 
         // Assert
-        assertThat(result.size()).isEqualTo(targetWord.getWord().length());
-        assertThat(result).isEqualTo(expectedResult);
+        assertThat(resultBingoStatuses.size()).isEqualTo(goalWord.getWord().length());
+        assertThat(resultBingoStatuses).isEqualTo(expectedBingoStatuses);
     }
 
-    private static Stream<Arguments> givenWordsAndTargetWordsAndExpectedBingoStatusList() {
+    private static Stream<Arguments> goalWordAndGivenWordAndExpectedBingoStatuses() {
         return Stream.of(
             Arguments.of(new Word("happy"), new Word("apple"), List.of(CONTAIN, CONTAIN, MATCH, NOTHING, NOTHING)),
             Arguments.of(new Word("heath"), new Word("melon"), List.of(NOTHING, MATCH, NOTHING, NOTHING, NOTHING)),
