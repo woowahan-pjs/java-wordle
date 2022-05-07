3ab6c4829289ac01375c40a5a95174ea2a6ca0e0
david <xodnjs2549@naver.com>
2022-05-07 21:56:41 +0900

    feat(TileLine): 생성자 매개변수 검증 메서드 추가

--- src/main/java/wordle/model/TileLine.java
+++ src/main/java/wordle/model/TileLine.java
@@ -1,14 +1,36 @@
 package wordle.model;
 
+import java.util.Objects;
+
 public class TileLine {
 
-	private TileStatus[] status;
+	private static final int SIZE_OF_STATUSES = 5;
+	private static final String WRONG_LENGTH_MESSAGE = "단어 매칭 결과 값은 5개여야 합니다";
+	private TileStatus[] statuses;
 
-	public TileLine(TileStatus[] status) {
-		this.status = status;
+	public TileLine(TileStatus[] tileStatuses) {
+		validate(tileStatuses);
+		this.statuses = tileStatuses;
+	}
+
+	private void validate(TileStatus[] tileStatuses) {
+		hasFiveStatus(tileStatuses);
+		isNotNull(tileStatuses);
+	}
+
+	private void isNotNull(TileStatus[] tileStatuses) {
+		for (TileStatus tileStatus : tileStatuses) {
+			Objects.requireNonNull(tileStatus);
+		}
+	}
+
+	private void hasFiveStatus(TileStatus[] tileStatuses) {
+		if (tileStatuses.length != SIZE_OF_STATUSES) {
+			throw new IllegalArgumentException(WRONG_LENGTH_MESSAGE);
+		}
 	}
 
 	public TileStatus[] getAllStatus() {
-		return status;
+		return statuses;
 	}
 }
