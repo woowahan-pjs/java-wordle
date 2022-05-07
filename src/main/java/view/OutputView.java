902a5e1b4deaa773bd219651748a0813ac3a3c4a
wenodev <logicalstark1105@gmail.com>
2022-05-08 02:18:17 +0900

    fix(OutputView.java) : answer는 전달하지 않는다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,11 +2,11 @@ package view;
 
 import domain.LetterResults;
 
+import java.util.List;
+
 public class OutputView {
     private static final String GAME_QUIT = "게임이 종료 되었습니다";
     private static final String NOT_EXIST = "단어 목록에 존해하지 않습니다";
-    private static final String ANSWER = "정답입니다";
-
 
     public static void quit(){
         System.out.println(GAME_QUIT);
@@ -16,11 +16,7 @@ public class OutputView {
         System.out.println(NOT_EXIST);
     }
 
-    public static void answer(){
-        System.out.println(ANSWER);
-    }
-
-    public static void result(LetterResults letterResults){
+    public static void result(List<LetterResults> letterResults){
         System.out.println(letterResults);
     }
 }
