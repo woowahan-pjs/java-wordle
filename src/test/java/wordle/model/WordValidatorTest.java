a22f705249eafdeb587f0b80c9f01c5ab87caf0d
david <xodnjs2549@naver.com>
2022-05-08 11:55:13 +0900

    test(WordValidatorTest): 검증 메서드 추가

--- src/test/java/wordle/model/WordValidatorTest.java
+++ src/test/java/wordle/model/WordValidatorTest.java
@@ -1,85 +1,55 @@
 package wordle.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import wordle.model.WordPool;
-import wordle.model.WordValidator;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class WordValidatorTest {
 
-	@DisplayName("입력된 단어가 5글자가 아니면 재입력을 요청한다")
-	@ParameterizedTest()
-	@CsvSource({"a", "aa", "aaa", "aaaa", "aaaaaa"})
-	void validateWordLength(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateWordLength", String.class);
-		method.setAccessible(true);
+	private static final String VALID_WORDS_TEXT_FILE_PATH = "src/test/resources/words.txt";
 
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput);
-
-		// then
-		assertThat(isValid).isFalse();
+	@ParameterizedTest
+	@ValueSource(chars = {'0', '!', 'ㅁ', 'な', '圓'})
+	@DisplayName("영문자가 아닌 문자는 유효하지 않다")
+	void Non_english_alphabet_is_invalid(char character) {
+		assertThat(WordValidator.isEnglishAlphabet(character)).isFalse();
 	}
 
-	@DisplayName("입력된 단어가 영단어가 아니면 재입력을 요청한다")
-	@ParameterizedTest()
-	@CsvSource({"1a2b3", "한글", "!@#ab", "AB 12"})
-	void validateAlphabet(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateAlphabet", String.class);
-		method.setAccessible(true);
-
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput);
-
-		// then
-		assertThat(isValid).isFalse();
+	@ParameterizedTest
+	@ValueSource(chars = {'a', 'b', 'c', 'd', 'e'})
+	@DisplayName("영문자만 유효하다")
+	void Only_english_alphabet_is_valid(char character) {
+		assertThat(WordValidator.isEnglishAlphabet(character)).isTrue();
 	}
 
-	@DisplayName("입력된 단어가 단어 목록에 없으면 실패이다")
 	@ParameterizedTest()
-	@CsvSource({"jason", "nosaj", "primm", "david"})
-	void validateNotExistedWord(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordPool wordPool = new WordPool(Arrays.asList("disco", "jelly", "table", "alert"));
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateExistedWord", String.class, WordPool.class);
-		method.setAccessible(true);
-
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput, wordPool);
-
-		// then
-		assertThat(isValid).isFalse();
+	@ValueSource(strings = {"a", "ab", "abc", "abcd", "abcdef"})
+	@DisplayName("5글자가 아닌 단어는 유효하지 않다")
+	void Word_has_not_five_letter_is_invalid(String word) {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> WordValidator.validateFiveLetterWord(word))
+			.withMessage(Message.INVALID_ENGLISH_ALPHABET_MESSAGE);
 	}
 
-	@DisplayName("입력된 단어가 단어 목록에 존재하면 성공이다")
 	@ParameterizedTest()
-	@CsvSource({"disco", "jelly", "table", "alert"})
-	void validateExistedWord(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordPool wordPool = new WordPool(Arrays.asList("disco", "jelly", "table", "alert"));
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateExistedWord", String.class, WordPool.class);
-		method.setAccessible(true);
+	@ValueSource(strings = {"aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"})
+	@DisplayName("단어 목록에 존재하지 않는 단어는 유효하지 않다")
+	void It_is_invalid_that_word_is_not_in_word_pool(String wordInWordPool) {
+		WordPool wordPool = WordPoolGenerator.generateFromFile(VALID_WORDS_TEXT_FILE_PATH);
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> WordValidator.isValid(wordInWordPool, wordPool))
+			.withMessage(Message.INVALID_WORD_MESSAGE);
+	}
 
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput, wordPool);
-
-		// then
-		assertThat(isValid).isTrue();
+	@ParameterizedTest()
+	@ValueSource(strings = {"1aaaa", "b!bbb", "ccㅁcc", "dddなd", "eeee圓"})
+	@DisplayName("단어는 영문자로만 구성되어야 한다")
+	void Word_has_only_english_letter(String wrongWord) {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> WordValidator.validateEnglishLetterOnly(wrongWord))
+			.withMessage(Message.INVALID_WORD_LENGTH_MESSAGE);
 	}
 }
