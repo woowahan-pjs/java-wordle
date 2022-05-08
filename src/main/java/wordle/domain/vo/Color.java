a07eaeb2185a253057ed1646596905a8077623f5
tongnamuu <thban33@naver.com>
2022-05-08 17:31:20 +0900

    feat: update enum

--- src/main/java/wordle/domain/vo/Color.java
+++ src/main/java/wordle/domain/vo/Color.java
@@ -1,7 +1,17 @@
 package wordle.domain.vo;
 
 public enum Color {
-    GREEN,
-    YELLOW,
-    GRAY;
+    GREEN("\uD83D\uDFE9"),
+    YELLOW("\uD83D\uDFE8"),
+    GRAY("\u2B1C");
+
+    private final String box;
+
+    Color(String box) {
+        this.box = box;
+    }
+
+    public String getBox() {
+        return box;
+    }
 }
