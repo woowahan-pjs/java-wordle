d11f38238b90daa87ba374b73b44113d7c3da81a
david <xodnjs2549@naver.com>
2022-05-08 18:30:30 +0900

    test(GameTest): 시도 횟수 내 정답을 맞추지 못한 결과 테스트 추가

--- src/test/java/wordle/model/GameTest.java
+++ src/test/java/wordle/model/GameTest.java
@@ -18,4 +18,35 @@ class GameTest {
 
 		assertThat(game.isFinish()).isTrue();
 	}
+
+	@Test
+	@DisplayName("시도 횟수 내 정답을 맞추지 못하면 게임은 종료된다")
+	void Game_is_over_with_over_trying_count() {
+		WordPool manipulatedWordPool = new ManipulatedWordPool();
+		Game game = new Game(manipulatedWordPool);
+		String wrongAnswerWord = "alone";
+		int MAXIMUM_TRYING_COUNT = 6;
+
+		for (int count = 0; count < MAXIMUM_TRYING_COUNT; count++) {
+			game.progressTurn(wrongAnswerWord);
+		}
+
+		assertThat(game.isFinish()).isTrue();
+	}
+
+	/**
+	 * 정답을 조작하기 위한 클래스
+	 */
+	static class ManipulatedWordPool implements WordPool {
+
+		@Override
+		public String getTodayAnswerWord() {
+			return "apple";
+		}
+
+		@Override
+		public boolean contains(String word) {
+			return false;
+		}
+	}
 }
