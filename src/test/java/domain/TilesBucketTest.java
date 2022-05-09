af9656cded6546cfe9c6a6729aab1311fdab977e
seungin yang <rhfpdk92@naver.com>
2022-05-10 01:43:52 +0900

    rename: 잘못된 테스트메서드 이름 변경

--- src/test/java/domain/TilesBucketTest.java
+++ src/test/java/domain/TilesBucketTest.java
@@ -18,7 +18,7 @@ class TilesBucketTest {
     }
 
     @Test
-    void words텍스트파일_존재하지않으면_에서_tiles를_읽어온다() {
+    void 존재하지_않는_words텍스트파일을_읽으면_에러를_발생한다() {
         assertThatThrownBy(() -> new TilesBucket("src/main/resources/not-exist-words.txt"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("words.txt를 찾을 수 없습니다.");
