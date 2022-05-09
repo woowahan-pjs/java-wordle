cdeb7d70c36c2d0fd6a34c9bd4eb7aa3e37096f8
minsoozz <minsoo1604@naver.com>
2022-05-09 23:43:16 +0900

    feat: Color code 추가

--- src/main/java/wordle/vo/Color.java
+++ src/main/java/wordle/vo/Color.java
@@ -1,7 +1,17 @@
 package wordle.vo;
 
 public enum Color {
-    GREEN,
-    YELLOW,
-    GRAY;
+  GREEN("\uD83D\uDFE9"),
+  YELLOW("\uD83D\uDFE8"),
+  GRAY("\u2B1C");
+
+  Color(String code) {
+    this.code = code;
+  }
+
+  private final String code;
+
+  public String getCode() {
+    return code;
+  }
 }
