992d662ef846a63e0815ed22df4ed2f5c98498dc
seungin yang <rhfpdk92@naver.com>
2022-05-10 01:21:10 +0900

    Test: assertThatIllegalArgumentException() 활용

--- src/test/java/domain/TilesTest.java
+++ src/test/java/domain/TilesTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class TilesTest {
 
@@ -20,9 +20,9 @@ public class TilesTest {
     @ParameterizedTest
     @ValueSource(strings = {"abc", "qwerty"})
     void Tiles는_5개의_Tile이_아닌경우_에러를_발생한다(String input) {
-        assertThatThrownBy(() -> new Tiles(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Tiles는 5개의 Tile로 구성되어야 한다.");
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Tiles(input))
+                .withMessageContaining("Tiles는 5개의 Tile로 구성되어야 한다.");
     }
 
     @Test
