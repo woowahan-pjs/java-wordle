837f053b03b662587f52734cd07e360879cdfa33
minsoozz <minsoo1604@naver.com>
2022-05-09 23:45:12 +0900

    feat: 결과 출력 기능 추가

--- src/main/java/wordle/vo/JudgeResult.java
+++ src/main/java/wordle/vo/JudgeResult.java
@@ -1,26 +1,36 @@
 package wordle.vo;
 
+import java.util.Arrays;
+
 public class JudgeResult {
-    Color[] result;
 
-    public JudgeResult() {
-        this.result = new Color[5];
-    }
+  Color[] result;
 
-    public Color[] getResult() {
-        return result;
-    }
+  public JudgeResult() {
+    this.result = new Color[5];
+  }
 
-    public void setColor(int idx, Color color) {
-        this.result[idx] = color;
-    }
+  public Color[] getResult() {
+    return result;
+  }
 
-    public boolean isEndAble() {
-        for (int i = 0; i < result.length; ++i) {
-            if (!result[i].equals(Color.GREEN)) {
-                return false;
-            }
-        }
-        return true;
+  public void setColor(int idx, Color color) {
+    this.result[idx] = color;
+  }
+
+  public boolean isEndAble() {
+    for (int i = 0; i < result.length; ++i) {
+      if (!result[i].equals(Color.GREEN)) {
+        return false;
+      }
     }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Arrays.stream(result).forEach(color -> sb.append(color.getCode()));
+    return sb.toString();
+  }
 }
