1e94fc3b88b4e2326e727c20497561c3c27ca54d
minsoozz <minsoo1604@naver.com>
2022-05-09 23:44:18 +0900

    fix: GameManager 완료

--- src/main/java/wordle/design/GameManager.java
+++ src/main/java/wordle/design/GameManager.java
@@ -1,6 +1,8 @@
 package wordle.design;
 
 public interface GameManager {
-    public void start(); //WORDLE을 6번 만에 맞춰 보세요. 시도의 결과는 타일의 색 변화로 나타납니다.
-    public void end(); // 임 게결과 출력
+
+  void start();
+
+  void end();
 }
