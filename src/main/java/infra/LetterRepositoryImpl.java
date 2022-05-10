edc74820aee4302288bcc0fc59766407a62a4cf0
wenodev <logicalstark1105@gmail.com>
2022-05-10 22:54:49 +0900

    fix(LetterRepositoryImpl.java) : NPE를 방지한다

--- src/main/java/infra/LetterRepositoryImpl.java
+++ src/main/java/infra/LetterRepositoryImpl.java
@@ -3,9 +3,7 @@ package infra;
 import domain.LetterRepository;
 import domain.Letters;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
@@ -13,7 +11,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LetterRepositoryImpl implements LetterRepository {
-    private static final String FILE_NAME = "words.txt";
+    private static final String FILE_ERROR = "words.txt 파일을 확인하세요. 변환에 문제가 생겼습니다.";
+    private static final String PATH = "src/main/resources/words.txt";
     private static final LocalDate criteriaDate = LocalDate.of(2021, 6, 19);
 
     public Letters getTodayAnswer(LocalDate today) {
@@ -29,13 +28,11 @@ public class LetterRepositoryImpl implements LetterRepository {
     }
 
     private List<Letters> findAll() {
-        URL resource = LetterRepositoryImpl.class.getClassLoader().getResource(FILE_NAME);
-        Path path = new File(resource.getPath()).toPath();
-        List<String> contents = null;
+        List<String> contents;
         try {
-            contents = Files.readAllLines(path);
+            contents = Files.readAllLines(Path.of(PATH));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new IllegalArgumentException(FILE_ERROR);
         }
 
         return contents.stream()
