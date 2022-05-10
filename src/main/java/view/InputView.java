1859dad23a7ba6d3a25fec51c29ece3f14dcefe7
wenodev <logicalstark1105@gmail.com>
2022-05-10 23:50:20 +0900

    fix(InputView.java) : 상수를 반영한다

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,6 @@ public class InputView {
     public static final String INPUT_GUIDE_MESSAGE = "문자를 입력하세요";
 
     public static void guide(){
-        System.out.println("문자를 입력하세요");
+        System.out.println(INPUT_GUIDE_MESSAGE);
     }
 }
