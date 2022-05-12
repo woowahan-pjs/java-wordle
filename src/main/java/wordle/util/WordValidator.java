8fbb08fa32180da0cb31a521b0e9b9c377b10665
Celina <luv.mins99@gmail.com>
2022-05-12 18:24:29 +0900

    fix(WordValidator): util이 view 영역에 접근 하는 부분 수정
    - view를 접근하지 않고 오류를 반환하도록 1차 개선
    - 2차 개선시에는 validate에서의 view 접근 로직도 수정하기

--- src/main/java/wordle/util/WordValidator.java
+++ src/main/java/wordle/util/WordValidator.java
@@ -6,36 +6,39 @@ import wordle.view.ConsoleOutput;
 public class WordValidator {
 
 	private static final int WORD_LENGTH = 5;
+	private static final String INVALID_LENGTH_MESSAGE = "단어는 5글자로 구성되어야 합니다.";
+	private static final String INVALID_ALPHABET_MESSAGE = "단어는 영문자로 구성되어야 합니다.";
 	private static final String WORD_PATTERN = "^[A-Za-z]+$";
+	private static final String INVALID_WORD_MESSAGE = "존재하지 않는 단어입니다.";
 
 	public static boolean validate(String userInput, WordPool wordPool) {
-		// 글자 수가 5자가 아닌 경우
-		if (!validateWordLength(userInput)) {
-			ConsoleOutput.printInvalidLengthMessage();
+		try {
+			validateWordLength(userInput);
+			validateAlphabet(userInput);
+			validateExistedWord(userInput, wordPool);
+		} catch (Exception e) {
+			ConsoleOutput.printErrorMessage(e.getMessage());
 			return false;
 		}
-		// 입력된 단어가 영문자로만 구성되어 있는지
-		if (!validateAlphabet(userInput)) {
-			ConsoleOutput.printInvalidAlphabetMessage();
-			return false;
-		}
-		// 단어 목록에 일치하는 단어가 있는지 없는지
-		if (!validateExistedWord(userInput, wordPool)) {
-			ConsoleOutput.printInvalidWordMessage();
-			return false;
-		}
+
 		return true;
 	}
 
-	private static boolean validateWordLength(String userInput) {
-		return userInput.length() == WORD_LENGTH;
+	private static void validateWordLength(String userInput) {
+		if (userInput.length() != WORD_LENGTH) {
+			throw new IllegalArgumentException(INVALID_LENGTH_MESSAGE);
+		}
 	}
 
-	private static boolean validateAlphabet(String userInput) {
-		return userInput.matches(WORD_PATTERN);
+	private static void validateAlphabet(String userInput) {
+		if (!userInput.matches(WORD_PATTERN)) {
+			throw new IllegalArgumentException(INVALID_ALPHABET_MESSAGE);
+		}
 	}
 
-	private static boolean validateExistedWord(String userInput, WordPool wordPool) {
-		return wordPool.contains(userInput);
+	private static void validateExistedWord(String userInput, WordPool wordPool) {
+		if (!wordPool.contains(userInput)) {
+			throw new IllegalArgumentException(INVALID_WORD_MESSAGE);
+		}
 	}
 }
--- src/main/java/wordle/view/ConsoleOutput.java
+++ src/main/java/wordle/view/ConsoleOutput.java
@@ -14,11 +14,8 @@ public class ConsoleOutput {
 	private static final int MAX_TRYING_COUNT = 6;
 	private static final String GAME_START_MESSAGE_FORMAT =
 		"WORDLE을 %d번 만에 맞춰 보세요." + NEW_LINE + "시도의 결과는 타일의 색 변화로 나타납니다." + NEW_LINE;
-	private static final String INVALID_WORD_MESSAGE = "존재하지 않는 단어입니다.";
 	private static final String TODAY_ANSWER_MESSAGE = "오늘의 정답 단어는 %s 입니다.";
 	private static final String TRYING_COUNT_MESSAGE_FORMAT = NEW_LINE + "%d" + DIVIDER + MAX_TRYING_COUNT + NEW_LINE;
-	private static final String INVALID_LENGTH_MESSAGE = "단어는 5글자로 구성되어야 합니다.";
-	private static final String INVALID_ALPHABET_MESSAGE = "단어는 영문자로 구성되어야 합니다.";
 
 
 	public void printGameStartMessage() {
@@ -42,16 +39,8 @@ public class ConsoleOutput {
 		}
 	}
 
-	public static void printInvalidLengthMessage() {
-		System.out.println(INVALID_LENGTH_MESSAGE);
-	}
-
-	public static void printInvalidAlphabetMessage() {
-		System.out.println(INVALID_ALPHABET_MESSAGE);
-	}
-
-	public static void printInvalidWordMessage() {
-		System.out.println(INVALID_WORD_MESSAGE);
+	public static void printErrorMessage(String errorMsg) {
+		System.out.println(errorMsg);
 	}
 
 	private void printGameResult(Tiles tile) {
