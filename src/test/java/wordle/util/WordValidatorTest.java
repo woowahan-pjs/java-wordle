e542c2853537c27f15975963e69461a1035ff8ff
Celina <luv.mins99@gmail.com>
2022-05-12 18:52:47 +0900

    test(WordValidatorTest): private 메서드에 대한 테스트 삭제
    - 로직을 개선함으로서 public 메서드 검증만으로 충분해졌다고 판단함

--- src/test/java/wordle/util/WordValidatorTest.java
+++ src/test/java/wordle/util/WordValidatorTest.java
@@ -1,6 +1,7 @@
 package wordle.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -16,112 +17,29 @@ class WordValidatorTest {
 	WordPool wordPool = new WordPool(WordPoolGenerator.generate());
 
 	@ParameterizedTest
-	@ValueSource(strings = {"world", "apple", "peace"})
-	void 유효한_단어가_입력되면_성공을_반환한다(String userInput) {
-		// given & when
-		boolean isValid = WordValidator.validate(userInput, wordPool);
-
-		// then
-		assertThat(isValid).isTrue();
-	}
-
-	@ParameterizedTest
 	@ValueSource(strings = {"", "a", "du", "test", "jaason"})
 	void 유효하지_않은_길이의_단어가_입력되면_오류를_반환한다(String userInput) {
-		// given & when
-		boolean isValid = WordValidator.validate(userInput, wordPool);
-
-		// then
-		assertThat(isValid).isFalse();
+		// given & when & then
+		assertThatIllegalArgumentException().
+			isThrownBy(() -> WordValidator.validate(userInput, wordPool))
+			.withMessageContaining("단어는 5글자로 구성되어야 합니다.");
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"%@$%#", "두두제이슨"})
 	void 영단어가_아닌_값이_입력되면_오류를_반환한다(String userInput) {
-		// given & when
-		boolean isValid = WordValidator.validate(userInput, wordPool);
-
-		// then
-		assertThat(isValid).isFalse();
+		// given & when & then
+		assertThatIllegalArgumentException().
+			isThrownBy(() -> WordValidator.validate(userInput, wordPool))
+			.withMessageContaining("단어는 영문자로 구성되어야 합니다.");
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"jason", "david", "books"})
 	void words_파일에_없는_값이_입력되면_오류를_반환한다(String userInput) {
-		// given & when
-		boolean isValid = WordValidator.validate(userInput, wordPool);
-
-		// then
-		assertThat(isValid).isFalse();
-	}
-
-	@DisplayName("입력된 단어가 5글자가 아니면 재입력을 요청한다")
-	@ParameterizedTest()
-	@CsvSource({"a", "aa", "aaa", "aaaa", "aaaaaa"})
-	void validateWordLength(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateWordLength", String.class);
-		method.setAccessible(true);
-
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput);
-
-		// then
-		assertThat(isValid).isFalse();
-	}
-
-	@DisplayName("입력된 단어가 영단어가 아니면 재입력을 요청한다")
-	@ParameterizedTest()
-	@CsvSource({"1a2b3", "한글", "!@#ab", "AB 12"})
-	void validateAlphabet(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateAlphabet", String.class);
-		method.setAccessible(true);
-
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput);
-
-		// then
-		assertThat(isValid).isFalse();
-	}
-
-	@DisplayName("입력된 단어가 단어 목록에 없으면 실패이다")
-	@ParameterizedTest()
-	@CsvSource({"jason", "nosaj", "primm", "david"})
-	void validateNotExistedWord(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordPool wordPool = new WordPool(Arrays.asList("disco", "jelly", "table", "alert"));
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateExistedWord", String.class, WordPool.class);
-		method.setAccessible(true);
-
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput, wordPool);
-
-		// then
-		assertThat(isValid).isFalse();
-	}
-
-	@DisplayName("입력된 단어가 단어 목록에 존재하면 성공이다")
-	@ParameterizedTest()
-	@CsvSource({"disco", "jelly", "table", "alert"})
-	void validateExistedWord(String normalUserInput)
-		throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		// given
-		WordPool wordPool = new WordPool(Arrays.asList("disco", "jelly", "table", "alert"));
-		WordValidator wordValidator = new WordValidator();
-		Method method = wordValidator.getClass().getDeclaredMethod("validateExistedWord", String.class, WordPool.class);
-		method.setAccessible(true);
-
-		// when
-		boolean isValid = (boolean) method.invoke(wordValidator, normalUserInput, wordPool);
-
-		// then
-		assertThat(isValid).isTrue();
+		// given & when & then
+		assertThatIllegalArgumentException().
+			isThrownBy(() -> WordValidator.validate(userInput, wordPool))
+			.withMessageContaining("존재하지 않는 단어입니다.");
 	}
 }
