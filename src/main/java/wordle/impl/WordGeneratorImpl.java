4d15ea9e69f7254094a6216de01a0896505b3218
minsoozz <minsoo1604@naver.com>
2022-05-14 19:26:48 +0900

    refactor: txt 파일 읽는 방법 변경

--- src/main/java/wordle/impl/WordGeneratorImpl.java
+++ src/main/java/wordle/impl/WordGeneratorImpl.java
@@ -1,51 +1,44 @@
 package wordle.impl;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import wordle.design.WordGenerator;
 
 public class WordGeneratorImpl implements WordGenerator {
 
-  private static final List<String> WORDS = readFile();
-  private static final String REFERENCE_DATE = "2021-06-19";
+    private static final List<String> WORDS = readFile();
+    private static final String REFERENCE_DATE = "2021-06-19";
 
-  @Override
-  public String execute() {
-    return WORDS.get(findIndex());
-  }
-
-  private static List<String> readFile() {
-
-    BufferedReader bufferedReader = null;
-    List<String> listOfLines = new ArrayList<>();
-
-    try {
-      bufferedReader = new BufferedReader(new FileReader("src/main/resources/words.txt"));
-
-      String line = bufferedReader.readLine();
-
-      while (line != null) {
-        listOfLines.add(line);
-        line = bufferedReader.readLine();
-      }
-
-    } catch (Exception e) {
-      e.printStackTrace();
+    @Override
+    public String execute() {
+        return WORDS.get(findIndex());
     }
-    return listOfLines;
-  }
 
-  private int findIndex() {
-    LocalDate now = LocalDate.now();
-    LocalDate referenceDay = LocalDate.parse(REFERENCE_DATE, DateTimeFormatter.ISO_LOCAL_DATE);
-    Duration diff = Duration.between(referenceDay.atStartOfDay(), now.atStartOfDay());
-    long diffDays = diff.toDays();
+    private static List<String> readFile() {
 
-    return (int) (diffDays % WORDS.size());
-  }
+        List<String> lines = null;
+
+        try {
+            lines = Files.lines(Path.of("src/main/resources/words.txt")).collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    private int findIndex() {
+        LocalDate now = LocalDate.now();
+        LocalDate referenceDay = LocalDate.parse(REFERENCE_DATE, DateTimeFormatter.ISO_LOCAL_DATE);
+        Duration diff = Duration.between(referenceDay.atStartOfDay(), now.atStartOfDay());
+        long diffDays = diff.toDays();
+
+        return (int) (diffDays % WORDS.size());
+    }
 }
