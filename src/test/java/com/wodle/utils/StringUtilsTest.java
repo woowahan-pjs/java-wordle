6ae85d38ed9e022d3b806d65f090bf4c52bfcaf7
jhsong2580 <thdwmdgns@naver.com>
2023-03-25 16:42:32 +0900

    Feat: StringUtils 기능 및 테스트코드 구현

---
+++ src/test/java/com/wodle/utils/StringUtilsTest.java
@@ -0,0 +1,29 @@
+package com.wodle.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StringUtilsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaa", "bbbbb", "ccccc", "ddddd"})
+    public void normalTest(String source) {
+        //when
+        boolean result = StringUtils.matchesFiveSmallAlphabet(source);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaa", "bbb", "cc", "d", "Aaaaa", "1aaaa", "ㅁaaaa"})
+    public void abNormalTest(String source) {
+        //when
+        boolean result = StringUtils.matchesFiveSmallAlphabet(source);
+
+        //then
+        assertThat(result).isFalse();
+    }
+}
