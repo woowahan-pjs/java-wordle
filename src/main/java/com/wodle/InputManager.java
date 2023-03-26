3a257c3511f472f120fd3373f8ec2ad27467689c
jhsong2580 <thdwmdgns@naver.com>
2023-03-26 15:15:30 +0900

    Feat: InputManager 기능 및 테스트코드 구현

--- src/main/java/com/wodle/InputManager.java
+++ src/main/java/com/wodle/InputManager.java
@@ -1,2 +1,27 @@
-package com.wodle;public class InputManager {
+package com.wodle;
+
+import static com.wodle.utils.StringUtils.matchesFiveSmallAlphabet;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+
+public class InputManager {
+
+    public Word inputWord() {
+        try {
+            String userInput = Console.readLine();
+            validate(userInput);
+            return new Word(userInput);
+        } catch (IllegalArgumentException e) {
+            return this.inputWord();
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("No such line");
+        }
+    }
+
+    private void validate(String userInput) {
+        if (!matchesFiveSmallAlphabet(userInput)) {
+            throw new IllegalArgumentException("user input require 5 small alphabet");
+        }
+    }
 }
---
+++ src/test/java/com/wodle/InputManagerTest.java
@@ -0,0 +1,47 @@
+package com.wodle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InputManagerTest {
+
+    private InputManager inputManager;
+
+    @BeforeEach
+    public void init() {
+        inputManager = new InputManager();
+    }
+
+
+    @Test
+    public void normalInputManagerTest() {
+        //given
+        inputSetting("abcde");
+
+        //when
+        Word word = inputManager.inputWord();
+
+        //then
+        assertThat(word.getWord()).isEqualTo("abcde");
+    }
+
+    @Test
+    public void abnormalInputManagerTest() {
+        //given
+        inputSetting("\nabcde");
+
+        //when
+        Word word = inputManager.inputWord();
+
+        //then
+        assertThat(word.getWord()).isEqualTo("abcde");
+    }
+
+    private void inputSetting(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
+    }
+}
