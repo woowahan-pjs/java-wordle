6c630faaed585ee3a1bb639a4369a25da7b3f996
jhsong2580 <thdwmdgns@naver.com>
2023-03-26 16:39:08 +0900

    feat: ViewManager 기능 구현 (색깔 네모 출력 구현 필요)

--- src/main/java/com/wodle/ViewManager.java
+++ src/main/java/com/wodle/ViewManager.java
@@ -1,2 +1,29 @@
-package com.wodle;public class ViewManager {
+package com.wodle;
+
+import com.wodle.domain.TileColor;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ViewManager {
+
+    private List<List<TileColor>> wordMatchResults;
+
+    public ViewManager() {
+        wordMatchResults = new LinkedList<>();
+    }
+
+    public void printResult(Result woreCompareResult) {
+        wordMatchResults.add(woreCompareResult.getMatchStatus());
+
+        for (List<TileColor> wordMatchResult : wordMatchResults) {
+            print(wordMatchResult);
+            System.out.println();
+        }
+    }
+
+    private void print(List<TileColor> curWordMatchResult) {
+        for (TileColor tileColor : curWordMatchResult) {
+            System.out.print(tileColor.name() + " ");
+        }
+    }
 }
