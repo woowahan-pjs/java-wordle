ea221ae39511905899e16e6fc26cb700575beec5
MoonHKLee <mf311110@gmail.com>
2023-03-27 00:10:00 +0900

    feat(wordProvider): Answer 호출 - 파일에서 요구사항 로직에 해당하는 단어를 가져오는 기능 구현.

--- src/main/java/domain/WordProvider.java
+++ src/main/java/domain/WordProvider.java
@@ -3,10 +3,20 @@ package domain;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 public class WordProvider {
-    public List<String> loadWordFile(String filePath) {
+    private final List<String> words;
+    private final LocalDate criteriaDate;
+
+    public WordProvider(String filePath, LocalDate criteriaDate) {
+        this.words = getWordList(filePath);
+        this.criteriaDate = criteriaDate;
+    }
+
+    public List<String> getWordList(String filePath) {
         Path path = Path.of(filePath);
         List<String> words;
         try {
@@ -18,5 +28,11 @@ public class WordProvider {
             throw new IllegalArgumentException("파일이 비어있습니다.");
         }
         return words;
+    }
+
+    public String getAnswer(LocalDate currentDate) {
+        Period period = Period.between(criteriaDate, currentDate);
+        int days = period.getDays();
+        return words.get(days % words.size());
     }
 }
--- src/test/java/domain/WordProviderTest.java
+++ src/test/java/domain/WordProviderTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,15 +13,15 @@ public class WordProviderTest {
     @Test
     @DisplayName("파일이 있으면 파일 로드 성공")
     void test() {
-        WordProvider wordProvider = new WordProvider();
-        wordProvider.loadWordFile("src/test/resources/test10words.txt");
+        WordProvider wordProvider = new WordProvider("src/test/resources/test10words.txt", LocalDate.now());
+        wordProvider.getWordList("src/test/resources/test10words.txt");
     }
 
     @Test
     @DisplayName("파일이 없으면 IllegalArgumentException throw")
     void fileNotFoundTest() {
-        WordProvider wordProvider = new WordProvider();
-        assertThatThrownBy(()->wordProvider.loadWordFile("/asfas.txt"))
+        WordProvider wordProvider = new WordProvider("src/test/resources/test10words.txt",LocalDate.now());
+        assertThatThrownBy(()->wordProvider.getWordList("/asfas.txt"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("파일 변환중에 err가 발생하였습니다.");
 
@@ -29,16 +30,16 @@ public class WordProviderTest {
     @Test
     @DisplayName("테스트 파일의 단어 갯수 테스트")
     void get10WordsTest() {
-        WordProvider wordProvider = new WordProvider();
-        List<String> words = wordProvider.loadWordFile("src/test/resources/test10words.txt");
+        WordProvider wordProvider = new WordProvider("src/test/resources/test10words.txt",LocalDate.now());
+        List<String> words = wordProvider.getWordList("src/test/resources/test10words.txt");
         assertThat(words.size()).isEqualTo(10);
     }
 
     @Test
     @DisplayName("테스트 파일 공백 테스트")
     void getEmptyWordsTest() {
-        WordProvider wordProvider = new WordProvider();
-        assertThatThrownBy(()->wordProvider.loadWordFile("src/test/resources/testEmptyWords.txt"))
+        WordProvider wordProvider = new WordProvider("src/test/resources/test10words.txt",LocalDate.now());
+        assertThatThrownBy(()->wordProvider.getWordList("src/test/resources/testEmptyWords.txt"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("파일이 비어있습니다.");
     }
@@ -46,6 +47,9 @@ public class WordProviderTest {
     @Test
     @DisplayName("현재날짜 - 2021년 6월 19일 % 배열의 크기 번재 단어를 정답으로 생성하는 테스트")
     void getAnswerByCriteriaDateTest() {
-
+        LocalDate today = LocalDate.of(2021, 6, 29);
+        WordProvider wordProvider = new WordProvider("src/test/resources/test10words.txt",today);
+        String answer = wordProvider.getAnswer(today);
+        assertThat(answer).isEqualTo("cigar");
     }
 }
