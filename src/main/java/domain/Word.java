01c5f7a95fbb662be94d93577604d4825f8110af
saerang <lsg10119@gamail.com>
2023-03-27 21:21:37 +0900

    feat: word 구현

---
+++ src/main/java/domain/Word.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Word {
+    public static final int WORD_LENGTH = 5;
+    private static final Pattern pattern = Pattern.compile("^[a-zA-Z]*");
+
+    private final String word;
+
+    public Word(String word) {
+        if (this.isNotMatchWord(word)) {
+            throw new IllegalArgumentException(word + "는 5글자의 알파벳이 아닙니다.");
+        }
+
+        this.word = word;
+    }
+
+    private boolean isNotMatchWord(String word){
+        return word.length() != WORD_LENGTH || !pattern.matcher(word).matches();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return Objects.equals(word, word1.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "word='" + word + '\'' +
+                '}';
+    }
+}
