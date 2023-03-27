30b683b7890d38150dadd4b55657abf5231f3cbf
yunbaek <yunbaek4564@gmail.com>
2023-03-27 22:35:02 +0900

    feat: 글자 값 객체 생성

---
+++ src/main/java/domain/Letter.java
@@ -0,0 +1,34 @@
+package domain;
+
+public class Letter {
+
+	private final char letter;
+
+	public Letter(char letter) {
+		if (isNotAlphabet(letter)) {
+			throw new IllegalArgumentException("문자는 영문자만 가능합니다.");
+		}
+		this.letter = letter;
+	}
+
+	private static boolean isNotAlphabet(char letter) {
+		return !((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Letter letter1 = (Letter)o;
+
+		return letter == letter1.letter;
+	}
+
+	@Override
+	public int hashCode() {
+		return letter;
+	}
+}
