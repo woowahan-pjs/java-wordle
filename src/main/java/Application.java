f3616e1d9df12937a19da58267987cdee13c8bde
koola97620 <koola97620@nate.com>
2023-03-28 22:16:40 +0900

    feat: Application 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
+import domain.WordleGame;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        WordleGame wordleGame = new WordleGame();
+        wordleGame.start();
     }
 }
