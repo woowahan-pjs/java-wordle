0825f3971cfc293f91076b5f7c40ab55537765e1
jeunghoonsong <thdwmdgns@naver.com>
2023-03-30 21:48:44 +0900

    fix: 문자 검증 로직 WORD로 이동

--- src/main/java/com/wodle/InputManager.java
+++ src/main/java/com/wodle/InputManager.java
@@ -9,18 +9,11 @@ public class InputManager {
     public Word inputWord() {
         try {
             String userInput = Console.readLine();
-            validate(userInput);
             return new Word(userInput);
         } catch (IllegalArgumentException e) {
             return this.inputWord();
         } catch (NoSuchElementException e) {
             throw new NoSuchElementException("No such line");
-        }
-    }
-
-    private void validate(String userInput) {
-        if (!matchesFiveSmallAlphabet(userInput)) {
-            throw new IllegalArgumentException("user input require 5 small alphabet");
         }
     }
 }
