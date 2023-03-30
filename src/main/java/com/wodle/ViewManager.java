5eb81ea22a65cd2db5b8b052f09c46258710c68d
jeunghoonsong <thdwmdgns@naver.com>
2023-03-30 22:10:07 +0900

    feat: 시작 및 단어 입력 전 출력 기능 구현

--- src/main/java/com/wodle/ViewManager.java
+++ src/main/java/com/wodle/ViewManager.java
@@ -10,10 +10,27 @@ public class ViewManager {
     private static final String FAIL_RESULT_TEXT = "실패 하셨습니다.";
     private static final String SUCCESS_RESULT_TEXT = "성공 하셨습니다.";
     private static final String TODAY_WORD_PREFIX = "오늘의 단어 ";
+    private static final String REQUEST_WORD_TEXT = "정답을 입력해 주세요.";
+    private static final String INPUT_WORD_ERROR_TEXT = "단어는 소문자 5개로 구성된 영단어입니다.";
+    private static final String GAME_INITIALIZING_TEXT = "WORDLE을 6번 만에 맞춰 보세요.\n"
+        + "시도의 결과는 타일의 색 변화로 나타납니다.";
+
     private final List<List<TileColor>> wordMatchResults;
 
     public ViewManager() {
         wordMatchResults = new LinkedList<>();
+    }
+
+    public void printGameStart() {
+        System.out.println(GAME_INITIALIZING_TEXT);
+    }
+
+    public void printRequestWordInput() {
+        System.out.println(REQUEST_WORD_TEXT);
+    }
+
+    public void notifyInvalidInputWord() {
+        System.out.println(INPUT_WORD_ERROR_TEXT);
     }
 
     public void printCompareResult(Result woreCompareResult) {
