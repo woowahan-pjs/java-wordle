0bff183e9ab77fe95d10a623fd0b37217b54c1dc
MoonHKLee <mf311110@gmail.com>
2023-03-30 22:57:24 +0900

    feat(Game) : Game 메인 메서드 생성

--- src/main/java/WordleApplication.java
+++ src/main/java/WordleApplication.java
@@ -1,5 +1,14 @@
+import controller.Game;
+import domain.AnswerGenerator;
+import view.InputView;
+
+import java.time.LocalDate;
+
 public class WordleApplication {
-	public static void main(String[] args) {
-		System.out.println("hello wordle");
-	}
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        AnswerGenerator answerGenerator = new AnswerGenerator("src/main/resources/words.txt");
+        Game game = new Game(inputView, answerGenerator);
+		game.start(LocalDate.now());
+    }
 }
