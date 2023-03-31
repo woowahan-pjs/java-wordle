b5025ed59d6c056fd5541c0d06aca5d9b78a1c06
jungpil98 <jungpil98@ajou.ac.kr>
2023-03-31 19:02:51 +0900

    feat(Words): 글자 및 단어 관련 도메인 작성 - 가안 작성을 위해, Main 클래스에 주석을 남겨두었습니다. - Word 단위로 알파벳 검증 위해 별도 클래스 분리

---
+++ src/main/java/wordle/Main.java
@@ -0,0 +1,31 @@
+package wordle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        // 동작하는 코드 만들기
+        // user input
+
+
+
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String inputData = br.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+
+
+        // 사용자로부터 입력 받기
+        // 검사
+        // 결과 출력
+        // 쪼개나가기
+
+
+    }
+}
