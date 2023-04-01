af78dea09360420ab6957410bac0c4c714fd7f30
juhyun <zzang9ha@naver.com>
2023-04-01 15:47:37 +0900

    feat: 사용자 단어와 정답 단어 비교 메서드 추가

--- src/main/java/domain/Wordles.java
+++ src/main/java/domain/Wordles.java
@@ -7,14 +7,16 @@ public class Wordles {
     private final List<Wordle> answer;
     private final List<Wordle> input;
 
-    public Wordles(String answer) {
+    public Wordles(String answer, String input) {
         this.answer = toWords(answer);
+        this.input = toWords(input);
     }
 
-    private List<Wordle> toWords(String answer) {
+    private List<Wordle> toWords(String word) {
+        // TODO: word 유효성 검증 추가
         List<Wordle> words = new ArrayList<>();
 
-        for (char ch : answer.toCharArray()) {
+        for (char ch : word.toCharArray()) {
             words.add(new Wordle(ch));
         }
 
@@ -22,7 +24,18 @@ public class Wordles {
     }
 
 
-    public boolean compare(String inputWord) {
+    public boolean isWordleCompleted() {
+        // 정답과 사용자의 입력 비교
+        int size = answer.size();
+        for (int i = 0; i < size; i++) {
+            if (isNotEqualWord(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    private boolean isNotEqualWord(int i) {
+        return answer.get(i).getWord() != input.get(i).getWord();
     }
 }
