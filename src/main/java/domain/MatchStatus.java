f06ae47753fccfe2bbd90a1b71e6cc4890d648b9
yunbaek <yunbaek4564@gmail.com>
2023-04-02 15:29:48 +0900

    feat(MatchStatus): 색상 코드 추가

--- src/main/java/domain/MatchStatus.java
+++ src/main/java/domain/MatchStatus.java
@@ -1,7 +1,18 @@
 package domain;
 
 public enum MatchStatus {
-	GRAY,
-	YELLOW,
-	GREEN
+	GRAY("⬜"),
+	YELLOW("\uD83D\uDFE8"),
+	GREEN("\uD83D\uDFE9");
+
+	private String unicode;
+
+	MatchStatus(String unicode) {
+		this.unicode = unicode;
+	}
+
+	@Override
+	public String toString() {
+		return unicode;
+	}
 }
---
+++ src/test/java/domain/MatchStatusTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MatchStatusTest {
+
+	@DisplayName("색상 출력 테스트")
+	@Test
+	void colorTest() {
+		MatchStatus green = MatchStatus.GREEN;
+		MatchStatus yellow = MatchStatus.YELLOW;
+		MatchStatus gray = MatchStatus.GRAY;
+
+		System.out.println("green = " + green);
+		System.out.println("yellow = " + yellow);
+		System.out.println("gray = " + gray);
+	}
+
+}
