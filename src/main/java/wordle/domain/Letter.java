7bbc8dd8c874abed0ac3a4a82a73ec2ec8f981c4
sang5c <skah321@gmail.com>
2023-04-02 11:43:45 +0900

    임시커밋

--- src/main/java/wordle/domain/Letter.java
+++ src/main/java/wordle/domain/Letter.java
@@ -3,19 +3,36 @@ package wordle.domain;
 import java.util.Objects;
 
 public class Letter {
-    private char ch;
+    // TODO: validate 0 < position < 5
+    private final int position;
+    private final char ch;
 
-    public Letter(char ch) {
+    public Letter(int position, char ch) {
         validateAlpha(ch);
         this.ch = ch;
+        this.position = position;
     }
 
     public char getValue() {
         return ch;
     }
 
+    // TODO: Result를 반환하는게 맞는지 고민 필요
+    public Result compare(Letter target) {
+        if (equals(target)) {
+            return Result.CORRECT;
+        }
+        if (hasSameValueAndDiffPosition(target)) {
+            return Result.HALF_CORRECT;
+        }
+        return Result.WRONG;
+    }
+
+    public boolean hasSameValueAndDiffPosition(Letter target) {
+        return this.position != target.position && this.ch == target.ch;
+    }
+
     private void validateAlpha(char ch) {
-        // Letter 라는 클래스가 하나의 글자
         if (ch > 'z' || ch < 'a') {
             throw new IllegalArgumentException("shoud be alphabet");
         }
@@ -26,11 +43,11 @@ public class Letter {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Letter letter = (Letter) o;
-        return ch == letter.ch;
+        return position == letter.position && ch == letter.ch;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ch);
+        return Objects.hash(position, ch);
     }
 }
