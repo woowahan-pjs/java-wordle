1699c55351dde3bcf5fce591a54054926b30ac31
MoonHKLee <mf311110@gmail.com>
2023-04-02 16:43:39 +0900

    refactor(Game) : Application에 노출된 filepath 은닉

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,7 @@ import controller.Game;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game("src/main/resources/words.txt");
+        Game game = new Game();
         game.start(LocalDate.now());
     }
 }
--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -11,16 +11,17 @@ import view.InputView;
 import view.OutputView;
 
 public class Game {
+	private static final String FILE_PATH = "src/main/resources/words.txt";
 	private final InputView inputView;
 	private final OutputView outputView;
 	private final AnswerGenerator answerGenerator;
 	private final Results results;
 	private int roundNumber;
 
-	public Game(String path) {
+	public Game() {
 		this.inputView = new InputView();
 		this.outputView = new OutputView();
-		this.answerGenerator = new AnswerGenerator(path);
+		this.answerGenerator = new AnswerGenerator(FILE_PATH);
 		this.results = new Results();
 		this.roundNumber = 0;
 	}
