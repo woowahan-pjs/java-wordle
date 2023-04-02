b08b65cae1cb6665232c776ca24e58d79e9738f7
MoonHKLee <mf311110@gmail.com>
2023-04-02 16:02:03 +0900

    fix(InputView) : 오타 수정.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
 	}
 
 	private static final String GAME_START_MESSAGE = "WORDLE을 6번 만에 맞춰보세요. \n시도의 결과는 타일의 색 변화로 나타납니다.";
-	private static final String INPUT_GUIDE_MESSAGE = "정답을 입력해주세요.";
+	private static final String INPUT_GUIDE_MESSAGE = "정답을 입력해 주세요.";
 
 	public void printStartMessage() {
 		System.out.println(GAME_START_MESSAGE);
