3058e52a1276e48b0193caebcbee4c716f9c8d22
MoonHKLee <mf311110@gmail.com>
2023-04-02 16:38:24 +0900

    fix(Application) : 기능 요구사항 반영.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 import controller.Game;
 
-public class WordleApplication {
+public class Application {
     public static void main(String[] args) {
         Game game = new Game("src/main/resources/words.txt");
         game.start(LocalDate.now());
