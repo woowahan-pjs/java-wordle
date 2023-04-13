eea56a0320a112d956f7da9af4b3ad4ec5c269ac
akayj820 <akayj820@gmail.com>
2023-04-13 23:33:39 +0900

    refactor: resource 파일명 상수로 선언

--- src/main/java/controller/WordleController.java
+++ src/main/java/controller/WordleController.java
@@ -14,9 +14,10 @@ import java.util.List;
 public class WordleController {
 
     private static final int PLAY_ROUND = 6;
+    private static final String WORDS_TXT = "words.txt";
 
     public static void main(String[] args) {
-        Words words = new Words(IOUtils.readFromResource("words.txt"));
+        Words words = new Words(IOUtils.readFromResource(WORDS_TXT));
 
         ResultView.startComent();
 
