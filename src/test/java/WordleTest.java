cc73074809c21a5306011fa39b7285c268bb3771
akayj820 <akayj820@gmail.com>
2023-04-13 23:46:21 +0900

    refactor: enum 상수에 대해 static import 추가

--- src/test/java/WordleTest.java
+++ src/test/java/WordleTest.java
@@ -1,19 +1,20 @@
 import domain.Color;
 import domain.Word;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ui.IOUtils;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static domain.Color.*;
+
 public class WordleTest {
     @Test
     void compare() {
         Word answer = new Word("spill");
         List<Color> colors = answer.compareWith(new Word("hello"));
-        Assertions.assertEquals(Arrays.asList(Color.GREY, Color.GREY, Color.YELLOW, Color.GREEN, Color.GREY), colors);
+        Assertions.assertEquals(Arrays.asList(GREY, GREY, YELLOW, GREEN, GREY), colors);
     }
 
     @Test
