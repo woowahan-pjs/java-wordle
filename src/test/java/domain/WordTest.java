b437aa16eec72213d79d00add3189311a0a2e3f1
jimbaemon <jimbae@kakao.com>
2023-04-15 19:33:05 +0900

    refactor: test 제 위치로 이동.

--- src/test/java/domain/WordTest.java
+++ src/test/java/domain/WordTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,5 +15,26 @@ public class WordTest {
 
         //when
         Assertions.assertThat(word.isContains(character)).isEqualTo(contain);
+    }
+
+    @Test
+    void compare() {
+        //given
+        Word answer = new Word("spill");
+
+        //when
+        Colors colors = answer.compareWith(new Word("hello"));
+
+        //then
+        Assertions.assertThat(colors).isEqualTo(
+            new Colors(Color.GREY, Color.GREY, Color.YELLOW, Color.GREEN, Color.GREY)
+        );
+    }
+    
+    @Test
+    void sizeCheck() {
+        Assertions.assertThatThrownBy(() -> new Words("aaaa"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("단어는 다섯글자로만 입력 가능합니다");
     }
 }
--- src/test/java/domain/WordsTest.java
+++ src/test/java/domain/WordsTest.java
@@ -1,3 +1,5 @@
+package domain;
+
 import domain.Word;
 import domain.Words;
 import java.time.LocalDate;
