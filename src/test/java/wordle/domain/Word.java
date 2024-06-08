ca2a8b3800e5a416ec1e2507f2a06b30e307917d
padoling <hysm920@gmail.com>
2024-06-08 11:18:48 +0900

    feat: wordList 주어진 단어 검증 로직 작성

---
+++ src/test/java/wordle/domain/Word.java
@@ -0,0 +1,24 @@
+package wordle.domain;
+
+import java.util.Objects;
+
+public class Word {
+    private String word;
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return Objects.equals(word, word1.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
+    }
+}
---
+++ src/test/java/wordle/domain/WordListTest.java
@@ -0,0 +1,38 @@
+package wordle.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WordListTest {
+
+    @Test
+    public void 주어진_단어가_wordList_안에_있으면_true를_반환한다() {
+        // given
+        Word word = new Word("cigar");
+        WordList wordList = new WordList(List.of(new Word("cigar")));
+
+        // when
+        boolean actual = wordList.contains(word);
+
+        // then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void 주어진_단어가_wordList_안에_없으면_false를_반환한다() {
+        // given
+        Word word = new Word("ooooo");
+        WordList wordList = new WordList(new ArrayList<>());
+
+        // when
+        boolean actual = wordList.contains(word);
+
+        // then
+        assertFalse(actual);
+    }
+}
