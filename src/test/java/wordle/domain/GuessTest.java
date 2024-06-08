52437cae2685645a40b125f74b216867e4fc3365
woozi <kwj1270@naver.com>
2024-06-08 11:57:14 +0900

    test: Guess에 소문자 문자만 들어오면 생성할 수 있다.

--- src/test/java/wordle/domain/GuessTest.java
+++ src/test/java/wordle/domain/GuessTest.java
@@ -2,9 +2,18 @@ package wordle.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GuessTest {
+
+    @Test
+    void 단어에_알파벳_소문자_문자가_들어오면_생성할_수_있다() {
+        // given
+        String word = "cigar";
+
+        assertDoesNotThrow(() -> new Guess(word));
+    }
 
     @Test
     void 단어에_알파벳_소문자가_아닌_문자가_들어오면_예외를_반환한다() {
@@ -15,3 +24,4 @@ public class GuessTest {
         assertThrows(RuntimeException.class, () -> new Guess(word));
     }
 }
+
