478f587527d978c84b5ced5c11eeb38cb8c43ddb
woozi <kwj1270@naver.com>
2024-06-08 11:25:28 +0900

    test: Selector가 주어진다면 조건에 해당하는 단어를_추출할 수 있다

--- src/test/java/wordle/domain/WordListTest.java
+++ src/test/java/wordle/domain/WordListTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WordListTest {
 
+
     @Test
-    public void 주어진_단어가_wordList_안에_있으면_true를_반환한다() {
+    void 주어진_단어가_wordList_안에_있으면_true를_반환한다() {
         // given
         Word word = new Word("cigar");
         WordList wordList = new WordList(List.of(new Word("cigar")));
@@ -24,7 +24,7 @@ public class WordListTest {
     }
 
     @Test
-    public void 주어진_단어가_wordList_안에_없으면_false를_반환한다() {
+    void 주어진_단어가_wordList_안에_없으면_false를_반환한다() {
         // given
         Word word = new Word("ooooo");
         WordList wordList = new WordList(new ArrayList<>());
@@ -34,5 +34,15 @@ public class WordListTest {
 
         // then
         assertFalse(actual);
+    }
+
+    @Test
+    void Selector가_주어진다면_조건에_해당하는_단어를_추출할_수_있다() {
+        Word word = new Word("cigar");
+
+        WordList wordList = new WordList(List.of(word));
+        Word actual = wordList.select(() -> word);
+
+        assertEquals(actual, word);
     }
 }
