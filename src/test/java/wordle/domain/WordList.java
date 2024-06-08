2f990b0f98b0f3dfb25dfb904857f7f58eb5e25b
padoling <hysm920@gmail.com>
2024-06-08 11:06:33 +0900

    test: wordList에 주어진 단어 없는지 검증 추가

--- src/test/java/wordle/domain/WordList.java
+++ src/test/java/wordle/domain/WordList.java
@@ -2,6 +2,7 @@ package wordle.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WordListTest {
@@ -17,5 +18,18 @@ public class WordListTest {
 
         // then
         assertTrue(actual);
+    }
+
+    @Test
+    public void 주어진_단어가_wordList_안에_없으면_false를_반환한다() {
+        // given
+        Word word = new Word("ooooo");
+        WordList wordList = new WordList();
+
+        // when
+        boolean actual = wordList.contains(word);
+
+        // then
+        assertFalse(actual);
     }
 }
