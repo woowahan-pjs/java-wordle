22494533a3c3b85b3ccd74474f13cb8699f75624
CHEESE <81912261+ychxexn@users.noreply.github.com>
2024-06-09 17:06:53 +0900

    feat: 라운드 별 결과에 따라 반복 로직 추가

--- src/main/java/kr/co/wordle/Application.java
+++ src/main/java/kr/co/wordle/Application.java
@@ -1,13 +1,23 @@
 package kr.co.wordle;
 
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         Answer answer = new Answer();
         Console console = new Console();
         console.init();
 
-        String input = console.inputView();
-        Round round = new Round(input);
-        round.roundResult(answer);
+        while (true) {
+            String input = console.inputView();
+            Round round = new Round(input);
+            Map<Tile, Integer> result = round.roundResult(answer);
+
+            if (result.getOrDefault(Tile.GREEN, 0) == 5) {
+                System.out.println("끝");
+                break;
+            }
+            System.out.println("다시");
+        }
     }
 }
--- src/main/java/kr/co/wordle/Round.java
+++ src/main/java/kr/co/wordle/Round.java
@@ -1,18 +1,25 @@
 package kr.co.wordle;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class Round {
 
     private final String input;
+    private final Map<Tile, Integer> result;
 
     public Round(String input) {
         this.input = input;
+        this.result = new EnumMap<>(Tile.class);
     }
 
-    public void roundResult(Answer answer) {
+    public Map<Tile, Integer> roundResult(Answer answer) {
         char[] inputChars = input.toCharArray();
         for (int i = 0; i < inputChars.length; i++) {
-            System.out.println(getTile(answer, i, inputChars[i]));
+            Tile key = getTile(answer, i, inputChars[i]);
+            result.compute(key, (k, v) -> (v == null) ? 1 : v + 1);
         }
+        return result;
     }
 
     public Tile getTile(Answer answer, int index, char target) {
