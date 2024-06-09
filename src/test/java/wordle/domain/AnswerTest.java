acdfe4933f59f56c9ff615495249ddd76be8fd39
padoling <hysm920@gmail.com>
2024-06-09 09:52:23 +0900

    test: 정답과 동일한 답안이 들어오면 모두 MATCHED인 결과를 반환하는 테스트 추가

---
+++ src/test/java/wordle/domain/AnswerTest.java
@@ -0,0 +1,25 @@
+package wordle.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AnswerTest {
+
+    @Test
+    void 정답은_정답과_동일한_답안이_들어오면_모두_MATCHED인_결과를_반환한다() {
+        // given
+        Answer answer = new Answer("cigar");
+        Guess guess = new Guess("cigar");
+
+        Result expectedResult = new Result(List.of(ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED));
+
+        // when
+        Result result = answer.examineResult(guess);
+
+        // then
+        assertEquals(result, expectedResult);
+    }
+}
