846ec026a3126fb63eca82e4d299e18f0b0c0360
hoa0217 <gjwjdghk123@naver.com>
2024-06-09 11:18:48 +0900

    fix(Result): equals hashCode 구현

--- src/main/java/wordle/domain/Result.java
+++ src/main/java/wordle/domain/Result.java
@@ -1,5 +1,7 @@
 package wordle.domain;
 
+import java.util.Objects;
+
 public class Result {
     private final Tile tile;
     private final Position position;
@@ -7,5 +9,22 @@ public class Result {
     public Result(Tile tile, int position) {
         this.tile = tile;
         this.position = new Position(position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Result result = (Result) o;
+        return tile == result.tile && Objects.equals(position, result.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tile, position);
     }
 }
--- src/test/java/wordle/domain/ResultTest.java
+++ src/test/java/wordle/domain/ResultTest.java
@@ -1,5 +1,6 @@
 package wordle.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
@@ -8,5 +9,12 @@ public class ResultTest {
     @Test
     void Result_생성_테스트() {
         assertDoesNotThrow(()->new Result(Tile.GREEN, 0));
+    }
+
+    @Test
+    void 타일과_위치가_같으면_동등한_객체이다() {
+        Result result = new Result(Tile.GREEN, 0);
+
+        assertThat(result).isEqualTo(new Result(Tile.GREEN, 0));
     }
 }
