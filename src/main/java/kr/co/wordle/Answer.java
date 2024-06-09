ca477cc5e7a3243614bb0ab5d71933b698e7f22d
CHEESE <81912261+ychxexn@users.noreply.github.com>
2024-06-09 16:47:52 +0900

    refactor: answer 클래스의 책임을 round 클래스로 분리

--- src/main/java/kr/co/wordle/Answer.java
+++ src/main/java/kr/co/wordle/Answer.java
@@ -8,21 +8,11 @@ public class Answer {
         this.value = AnswerProvider.todayAnswer();
     }
 
-    public void roundResult(String input) {
-        char[] answerChars = value.toCharArray();
-        char[] inputChars = input.toCharArray();
-        for (int i = 0; i < answerChars.length; i++) {
-            System.out.println(getTile(answerChars[i], inputChars[i]));
-        }
+    public char charAt(int index) {
+        return value.charAt(index);
     }
 
-    public Tile getTile(char source, char target) {
-        if (source == target) {
-            return Tile.GREEN;
-        }
-        if (value.indexOf(target) > -1) {
-            return Tile.YELLOW;
-        }
-        return Tile.GRAY;
+    public boolean contains(char target) {
+        return value.indexOf(target) > -1;
     }
 }
--- src/main/java/kr/co/wordle/Application.java
+++ src/main/java/kr/co/wordle/Application.java
@@ -2,10 +2,12 @@ package kr.co.wordle;
 
 public class Application {
     public static void main(String[] args) {
+        Answer answer = new Answer();
         Console console = new Console();
         console.init();
+
         String input = console.inputView();
-        Answer answer = new Answer();
-        answer.roundResult(input);
+        Round round = new Round(input);
+        round.roundResult(answer);
     }
 }
