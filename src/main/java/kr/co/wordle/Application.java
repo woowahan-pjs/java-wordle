0f37271ee47d4ee64a3ba9e26eb66009c1415f65
CHEESE <81912261+ychxexn@users.noreply.github.com>
2024-06-09 16:03:43 +0900

    feat: 게임 시작 입출력 클래스 생성

---
+++ src/main/java/kr/co/wordle/Application.java
@@ -0,0 +1,9 @@
+package kr.co.wordle;
+
+public class Application {
+    public static void main(String[] args) {
+        Console console = new Console();
+        console.init();
+        console.inputView();
+    }
+}
---
+++ src/main/java/kr/co/wordle/Console.java
@@ -0,0 +1,16 @@
+package kr.co.wordle;
+
+import java.util.Scanner;
+
+public class Console {
+
+    public void init() {
+        System.out.println("WORDLE을 6번 만에 맞춰 보세요.\n시도의 결과는 타일의 색 변화로 나타납니다.\n");
+    }
+
+    public String inputView() {
+        System.out.println("정답을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
