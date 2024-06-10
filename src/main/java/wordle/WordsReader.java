bbc228085e8864b77a6cadda5966041382df7462
김명환 <samkim2626@joonggonara.co.kr>
2024-06-10 21:57:00 +0900

    feat: 파일 읽기 로직 구현

---
+++ src/main/java/wordle/WordsReader.java
@@ -0,0 +1,19 @@
+package wordle;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class WordsReader {
+
+    public List<String> read() {
+        Path path = Path.of("src/main/resources/words.txt");
+
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new RuntimeException("파일 읽기에 실패했습니다.");
+        }
+    }
+}
