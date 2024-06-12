5c08e858d26309b6524a4ac5b35e1d4391ac85b4
hoa0217 <gjwjdghk123@naver.com>
2024-06-12 22:40:55 +0900

    feat(FileReader): FileReader 구현
    - resource 밑에 있는 파일을 읽어온다

---
+++ src/test/java/wordle/infra/FileReaderTest.java
@@ -0,0 +1,37 @@
+package wordle.infra;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import wordle.exception.FileReadFailException;
+
+public class FileReaderTest {
+
+    private FileReader fileReader;
+
+    @BeforeEach
+    public void setUp() {
+        fileReader = new FileReader();
+    }
+
+    @Test
+    void FileReader로_텍스트파일을_읽어온다() {
+        List<String> textList = fileReader.readByLine("words.txt");
+
+        assertThat(textList).containsExactly("apple","hello","abcde","spike","happy");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"noExist.txt"})
+    void FileReader로_없는_파일을_읽어오면_예외를_던진다(final String input) {
+        assertThatThrownBy(() -> fileReader.readByLine(input))
+                .isInstanceOf(FileReadFailException.class);
+    }
+}
