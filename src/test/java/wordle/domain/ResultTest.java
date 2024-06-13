20c1ee4927ae1af091fa8aabe259b51f85c20687
woozi <kwj1270@naver.com>
2024-06-13 20:48:06 +0900

    test: 모든 ResultType이 Matched가 아니라면 매칭 성공 여부를 false로 반환한다

--- src/test/java/wordle/domain/ResultTest.java
+++ src/test/java/wordle/domain/ResultTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ResultTest {
@@ -13,5 +14,12 @@ public class ResultTest {
         Result result = new Result(List.of(ResultType.MATCHED, ResultType.MATCHED));
 
         assertTrue(result.allMatched());
+    }
+
+    @Test
+    void 모든_ResultType이_Matched가_아니라면_매칭_성공_여부를_false로_반환한다() {
+        Result result = new Result(List.of(ResultType.MISMATCHED, ResultType.MATCHED));
+
+        assertFalse(result.allMatched());
     }
 }
