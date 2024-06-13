9787aef7dcddfa122dcf48a7984dc52bf71c437b
woozi <kwj1270@naver.com>
2024-06-13 21:53:38 +0900

    test: 소문자가 아닌 단어가 들어온다면 Alphabet을 생성할 수 없다

--- src/test/java/wordle/domain/AlphabetTest.java
+++ src/test/java/wordle/domain/AlphabetTest.java
@@ -2,12 +2,21 @@ package wordle.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AlphabetTest {
 
     @Test
     void 소문자_영단어로_Alphabet을_생성할_수_있다() {
         assertDoesNotThrow(() -> Alphabet.of('a'));
+    }
+
+    @Test
+    void 소문자가_아닌_단어가_들어온다면_Alphabet을_생성할_수_없다() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> Alphabet.of('A')),
+                () -> assertThrows(IllegalArgumentException.class, () -> Alphabet.of('1')),
+                () -> assertThrows(IllegalArgumentException.class, () -> Alphabet.of('ㄱ'))
+        );
     }
 }
