9f7d1c062136538c3418e88f81fe0deac7d73cf3
padoling <hysm920@gmail.com>
2024-06-13 20:39:07 +0900

    test : 문자가 모두 일치하는 결과가 있는 경우 true를 반환한다.

---
+++ src/test/java/wordle/domain/ResultsTest.java
@@ -0,0 +1,26 @@
+package wordle.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ResultsTest {
+
+    @Test
+    void 문자가_모두_일치하는_결과가_있는_경우_true를_반환한다() {
+        // given
+        List<Result> resultList = List.of(
+                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED))
+        );
+        Results results = new Results(resultList);
+
+        // when
+        results.hasAnswer();
+
+        // then
+        assertTrue(results.hasAnswer());
+    }
+}
