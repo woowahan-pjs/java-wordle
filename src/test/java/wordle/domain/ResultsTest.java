924d38ca86e96b947b4820caf21e335256f4ad05
padoling <hysm920@gmail.com>
2024-06-13 20:39:27 +0900

    test : 문자가 모두 일치하는 결과가 없는 경우 false를 반환한다.

--- src/test/java/wordle/domain/ResultsTest.java
+++ src/test/java/wordle/domain/ResultsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ResultsTest {
@@ -22,5 +23,21 @@ public class ResultsTest {
 
         // then
         assertTrue(results.hasAnswer());
+    }
+
+    @Test
+    void 문자가_모두_일치하는_결과가_없는_경우_false를_반환한다() {
+        // given
+        List<Result> resultList = List.of(
+                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MATCHED, ResultType.MISMATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED))
+        );
+        Results results = new Results(resultList);
+
+        // when
+        results.hasAnswer();
+
+        // then
+        assertFalse(results.hasAnswer());
     }
 }
