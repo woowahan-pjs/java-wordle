41c538d38027a3d791587c2540e3ff4b1d30e692
jylim <jylim@12cm.co.kr>
2024-06-13 09:41:42 +0900

    chore: Scacnner추가

--- src/main/java/ui/HintView.java
+++ src/main/java/ui/HintView.java
@@ -1,9 +1,14 @@
 package ui;
 
 import domain.MatchResults;
+import java.util.Scanner;
 
 public class HintView {
     public void render(MatchResults matchResults) {
+//            Scanner sc = new Scanner(System.in);
+//            return sc.nextLine();
+
         System.out.println("render grid");
     }
 }
+1
