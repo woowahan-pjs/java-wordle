48ed3b113c34fd59ebbb10e17b557c60890c0026
woozi <kwj1270@naver.com>
2024-06-13 21:51:30 +0900

    feat: 소문자 영단어로 Alphabet을 생성할 수 있다

---
+++ src/test/java/wordle/domain/Alphabet.java
@@ -0,0 +1,18 @@
+package wordle.domain;
+
+import java.util.Arrays;
+
+public enum Alphabet {
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+
+    public static Alphabet of(char alphabet) {
+        return of(String.valueOf(alphabet));
+    }
+
+    public static Alphabet of(String alphabet) {
+        return Arrays.stream(values())
+                .filter(it -> it.name().equals(alphabet))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
