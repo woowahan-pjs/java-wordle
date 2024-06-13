ea46eb5ccc8c64c81ad8ac3b20d455b127b83e5f
woozi <kwj1270@naver.com>
2024-06-13 21:59:41 +0900

    refactor: Word 내부 의존성을 Alphabet 리스트로 변경

--- src/test/java/wordle/domain/Word.java
+++ src/test/java/wordle/domain/Word.java
@@ -1,48 +1,48 @@
 package wordle.domain;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Word {
     public static final int WORD_SIZE = 5;
 
-    private String word;
+    private List<Alphabet> alphabets;
 
     public Word(String word) {
         validate(word);
-        this.word = word;
+        this.alphabets = word.chars()
+                .mapToObj(it -> (char) it)
+                .map(Alphabet::of)
+                .toList();
     }
 
     public String getWord() {
-        return word;
+        return alphabets.stream()
+                .map(Alphabet::name)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
     }
 
     private static void validate(final String word) {
         if (word.trim().length() != WORD_SIZE) {
             throw new RuntimeException();
         }
-        boolean result = word.chars()
-                .mapToObj(c -> (char) c)
-                .allMatch(c -> c >= 'a' && c <= 'z');
-
-        if (!result) {
-            throw new RuntimeException();
-        }
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Word word1 = (Word) o;
-        return Objects.equals(word, word1.word);
+        final Word word = (Word) o;
+        return Objects.equals(alphabets, word.alphabets);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(word);
+        return Objects.hashCode(alphabets);
     }
 
     public boolean isSameWord(final String word) {
-        return this.word.equals(word);
+        return this.equals(new Word(word));
     }
 }
