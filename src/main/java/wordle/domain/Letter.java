7c56c0a9246fcf44916b617d93efdf3daea894af
hoa0217 <gjwjdghk123@naver.com>
2024-06-13 21:36:40 +0900

    refactor(Letter): Alphabet을 값객체(VO)로 갖는다.

--- src/main/java/wordle/domain/Letter.java
+++ src/main/java/wordle/domain/Letter.java
@@ -4,12 +4,20 @@ import java.util.Objects;
 
 public class Letter {
 
-    private char alphabet;
-    private Position position;
+    private final Alphabet alphabet;
+    private final Position position;
 
-    public Letter(char alphabet, int position) {
-        this.alphabet = alphabet;
+    public Letter(final char alphabet, final int position) {
+        this.alphabet = new Alphabet(alphabet);
         this.position = new Position(position);
+    }
+
+    public boolean isSameAlphabet(Letter letter) {
+        return letter.alphabet.equals(this.alphabet);
+    }
+
+    public Position getPosition() {
+        return position;
     }
 
     @Override
@@ -21,19 +29,12 @@ public class Letter {
             return false;
         }
         Letter letter = (Letter) o;
-        return alphabet == letter.alphabet && Objects.equals(position, letter.position);
+        return Objects.equals(alphabet, letter.alphabet) && Objects.equals(position,
+                letter.position);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(alphabet, position);
-    }
-
-    public boolean isSameAlphabet(Letter letter) {
-        return letter.alphabet == this.alphabet;
-    }
-
-    public Position getPosition() {
-        return position;
     }
 }
