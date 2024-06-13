74acae5eb650e66412c931b018253439a50136be
hoa0217 <gjwjdghk123@naver.com>
2024-06-13 21:20:50 +0900

    feat(Alphabet): Alphabet 클래스 생성
    - 알파벳을 소문자로 저장하는 클래스

---
+++ src/main/java/wordle/domain/Alphabet.java
@@ -0,0 +1,35 @@
+package wordle.domain;
+
+import java.util.Objects;
+import wordle.exception.InvalidAlphabetException;
+
+public class Alphabet {
+
+    private final char alphabet;
+
+    public Alphabet(char alphabet) {
+        char lowerAlphabet = Character.toLowerCase(alphabet);
+        if (lowerAlphabet < 'a' || lowerAlphabet > 'z') {
+            throw new InvalidAlphabetException();
+        }
+
+        this.alphabet = alphabet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Alphabet alphabet1 = (Alphabet) o;
+        return alphabet == alphabet1.alphabet;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(alphabet);
+    }
+}
