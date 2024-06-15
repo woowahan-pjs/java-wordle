6ca7944750ef1b57f23f2080163c257880e4e39e
hoa0217 <gjwjdghk123@naver.com>
2024-06-15 15:43:59 +0900

    test(WordBook): 시간관련 의존성 제거

--- src/test/java/wordle/domain/FileWordBookTest.java
+++ src/test/java/wordle/domain/FileWordBookTest.java
@@ -3,12 +3,10 @@ package wordle.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import wordle.TimeTestSupporter;
 import wordle.infra.FileReader;
 
 public class FileWordBookTest {
@@ -28,20 +26,10 @@ public class FileWordBookTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "2024-06-08:apple",
-            "2024-06-09:hello",
-            "2024-06-10:abcde",
-            "2024-06-11:spike",
-            "2024-06-12:happy"
-    }, delimiter = ':')
-    void WordBook에서_공식을_통해_오늘의_정답_단어를_뽑는다(LocalDate mockDate, String expected) {
-        TimeTestSupporter.runWithMock(mockDate, () -> {
-            Word actual = wordBook.pick(new BaseAnswerFormula());
-
-            assertThat(actual).isEqualTo(new Word(expected));
-        });
+    @Test
+    void WordBook에서_공식을_통해_오늘의_정답_단어를_뽑는다() {
+        Word actual = wordBook.pick((wordCount) -> 0);
+        assertThat(actual).isEqualTo(new Word("apple"));
     }
 
     @Test
