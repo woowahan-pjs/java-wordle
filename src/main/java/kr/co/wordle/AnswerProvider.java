f59a5ef9881c217dfd7e96d34eb69939c08ecfc9
CHEESE <81912261+ychxexn@users.noreply.github.com>
2024-06-15 17:50:14 +0900

    refactor: 입력값 검증 로직 InputValidator 클래스로 분리

--- src/main/java/kr/co/wordle/AnswerProvider.java
+++ src/main/java/kr/co/wordle/AnswerProvider.java
@@ -1,16 +1,9 @@
 package kr.co.wordle;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Collections;
 import java.util.List;
 
-import static kr.co.wordle.WordleGameConfig.WORDS_FILE_PATH;
 
 public class AnswerProvider {
 
@@ -19,20 +12,8 @@ public class AnswerProvider {
     private AnswerProvider() {
     }
 
-    private static List<String> readWordsInFile() {
-        try {
-            URL resource = AnswerProvider.class.getClassLoader().getResource(WORDS_FILE_PATH);
-            if (resource != null) {
-                return Files.readAllLines(Paths.get(resource.toURI()));
-            }
-        } catch (URISyntaxException | IOException e) {
-            e.printStackTrace();
-        }
-        return Collections.emptyList();
-    }
-
     public static String todayAnswer() {
-        List<String> words = readWordsInFile();
+        List<String> words = WordFileReader.readWordsInFile();
         int dayDiff = dayDiff();
         return words.get(dayDiff % words.size());
     }
@@ -44,7 +25,7 @@ public class AnswerProvider {
     }
 
     public static boolean isInputInWords(String input) {
-        List<String> words = readWordsInFile();
+        List<String> words = WordFileReader.readWordsInFile();
         return words.contains(input);
     }
 }
