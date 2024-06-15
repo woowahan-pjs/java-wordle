c1b4bb91e6773b5ac15fa9c0cfe313f985c1199d
CHEESE <81912261+ychxexn@users.noreply.github.com>
2024-06-15 17:16:50 +0900

    feat: 게임 관련 상수를 관리하는 클래스 생성

---
+++ src/main/java/kr/co/wordle/WordleGameConfig.java
@@ -0,0 +1,10 @@
+package kr.co.wordle;
+
+public class WordleGameConfig {
+    public static final String WORDS_FILE_PATH = "words.txt";
+    public static final int WORD_LENGTH = 5;
+    public static final int MAX_ROUND = 6;
+
+    private WordleGameConfig() {
+    }
+}
