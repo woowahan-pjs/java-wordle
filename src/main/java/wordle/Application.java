cc4afbf3e1692db9616aff39d54cc276198e5340
hoa0217 <gjwjdghk123@naver.com>
2024-06-15 16:52:15 +0900

    feat(Wordle): 게임완성

--- src/main/java/wordle/Application.java
+++ src/main/java/wordle/Application.java
@@ -1,7 +1,21 @@
 package wordle;
 
+import wordle.application.Wordle;
+import wordle.domain.FileWordBook;
+import wordle.domain.WordBook;
+import wordle.infra.FileReader;
+import wordle.ui.ConsoleInputView;
+import wordle.ui.ConsoleOutputView;
+import wordle.ui.InputView;
+import wordle.ui.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        WordBook wordBook = new FileWordBook(new FileReader());
+        InputView inputView = new ConsoleInputView();
+        OutputView outputView = new ConsoleOutputView();
+        Wordle wordle = new Wordle(wordBook, inputView, outputView);
+        wordle.startGame();
     }
 }
---
+++ src/main/java/wordle/application/Wordle.java
@@ -0,0 +1,68 @@
+package wordle.application;
+
+import wordle.domain.AnswerFormula;
+import wordle.domain.BaseAnswerFormula;
+import wordle.domain.Record;
+import wordle.domain.Results;
+import wordle.domain.Word;
+import wordle.domain.WordBook;
+import wordle.ui.InputView;
+import wordle.ui.OutputView;
+
+public class Wordle {
+
+    private final WordBook wordBook;
+
+    private final InputView inputView;
+
+    private final OutputView outputView;
+
+    private final Record record;
+
+    private final AnswerFormula answerFormula;
+
+    public Wordle(WordBook wordBook, InputView inputView, OutputView outputView) {
+        this.wordBook = wordBook;
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.record = new Record();
+        this.answerFormula = new BaseAnswerFormula();
+    }
+
+    public void startGame() {
+        Word answerWord = wordBook.pick(answerFormula);
+        outputView.welcome();
+        runGame(answerWord);
+        concludeGame();
+    }
+
+    private void runGame(Word answerWord) {
+        while (!record.isEnd()) {
+            outputView.showRecord(record);
+            handleWrongAnswer(() -> processTurn(answerWord));
+        }
+    }
+
+    private void processTurn(Word answerWord) {
+        outputView.askAnswer();
+        Word inputWord = wordBook.find(inputView.input());
+        Results results = answerWord.compare(inputWord);
+        record.add(results);
+    }
+
+    private void concludeGame() {
+        if (record.existAllGreen()) {
+            outputView.successEnd(record);
+            return;
+        }
+        outputView.failEnd(record);
+    }
+
+    private void handleWrongAnswer(Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            outputView.wrongAnswer();
+        }
+    }
+}
