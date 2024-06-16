2b28b8dc64005df93f0eab532087e567b52011ab
jylim <jylim@12cm.co.kr>
2024-06-16 11:22:16 +0900

    feat: 게임 기능 구현

--- src/main/java/controller/GameManager.java
+++ src/main/java/controller/GameManager.java
@@ -1,8 +1,13 @@
 package controller;
 
 import domain.Answer;
+import domain.InputWord;
 import domain.MatchResults;
 import domain.Round;
+import ui.GuideTextView;
+import ui.HintView;
+import ui.InputView;
+import ui.RoundView;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,21 +17,56 @@ public class GameManager {
     private Round round;
     private List<MatchResults> matchResults;
     private Answer answer;
-    public GameManager() {
+
+    private List<String> availableWords;
+
+    private GuideTextView guideTextView;
+    private InputView inputView;
+    private HintView hintView;
+    private RoundView roundView;
+    private boolean isEndGame = false;
+
+    protected GameManager() {
         this.matchResults = new ArrayList<>();
+        this.availableWords = new ArrayList<>();
     }
 
-    public GameManager(List<String> wordList) {
-        this.round = new Round(5, 0);       // 총 6번과 시작은 항상 0
+    public GameManager(List<String> availableWords) {
+        this.answer = new Answer(LocalDate.now(), availableWords);
+        this.availableWords = availableWords;
+        this.round = new Round(6, 1);
         this.matchResults = new ArrayList<>();
-        this.answer = new Answer(LocalDate.now(), wordList);
-    }
-
-    public void startGame() {
-
+        this.guideTextView = new GuideTextView();
+        this.inputView = new InputView();
+        this.hintView = new HintView();
+        this.roundView = new RoundView();
     }
 
 
     public void start() {
+        guideTextView.render();
+        while(!this.isEndGame && round.isNotFinalRound()) {
+            startRound();
+        }
+    }
+
+    private void startRound() {
+        // 라운드 입력 view
+        String input = inputView.input();
+
+        InputWord inputWord = new InputWord(input, this.availableWords);
+        MatchResults matchResultOfInput = inputWord.match(answer);
+        this.matchResults.add(matchResultOfInput);
+
+        // 정답 확인
+        boolean isEndGame = matchResultOfInput.isEndGame();
+        if(isEndGame) {
+            roundView.render(round.getCurrent(),round.getLimit());
+            this.isEndGame = isEndGame;
+        }
+
+        // 힌트 노출
+        hintView.render(this.matchResults);
+        round.goNext();
     }
 }
