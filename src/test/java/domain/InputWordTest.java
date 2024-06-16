aab91e415f0dc5a35ae54c9684849e4a0280d388
jylim <jylim@12cm.co.kr>
2024-06-16 15:41:39 +0900

    refactor(test): 변경된 구조에 맞게 test 로직 변경

--- src/test/java/domain/InputWordTest.java
+++ src/test/java/domain/InputWordTest.java
@@ -1,15 +1,10 @@
-
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -17,7 +12,7 @@ class InputWordTest {
     @DisplayName("입력단어 유효성 검증 성공 테스트")
     void validateSuccessInputWord() {
         List<String> words = List.of("apples", "cherry");
-        assertDoesNotThrow(() -> new InputWord("cherry", words));
+        assertDoesNotThrow(() -> Word.createInput("cherry", words));
 
     }
 
@@ -27,44 +22,27 @@ class InputWordTest {
     void
     validateFailInputWord() {
         List<String> words = List.of("apples", "banana");
-        assertThrows(IllegalArgumentException.class,() -> new InputWord("pangyo", words));
+        assertThrows(IllegalArgumentException.class,() -> Word.createInput("pangyo", words));
     }
 
     @Test
-    @DisplayName("입력단어와 정답 비교")
-    void matchTest() {
-        List<String> words = List.of("apples", "banana", "cherry");
-
-        InputWord inputWord = new InputWord("cherry", words);
-        Answer answer = new Answer("zzzrxx");
-        MatchResults matchResults = inputWord.match(answer);
-
-        assertThat(matchResults.getResults()).containsExactly(
-                new MatchResult('c', Hint.CORRECT),
-                new MatchResult('h', Hint.EXIST),
-                new MatchResult('e', Hint.NOT_EXIST),
-                new MatchResult('r', Hint.NOT_EXIST),
-                new MatchResult('r', Hint.NOT_EXIST),
-                new MatchResult('y', Hint.NOT_EXIST)
-        );
+    @DisplayName("입력단어 글자수 유효성 검증 실패 테스트")
+    void validateInputWordLength() {
+        List<String> words = List.of("apple", "abcdef");
+        assertThrows(IllegalArgumentException.class,() -> Word.createInput("abcdef", words));
     }
+
 
     @Test
-    @DisplayName("입력단어와 정답 위치 확인 테스트")
-    void matchLocationTest() {
-        List<String> words = List.of("xxxrxr");
+    @DisplayName("입력단어 영단어 유효성 검증 실패 테스트")
+    void validateInputWordOnlyEnglish() {
+        List<String> words = List.of("apple", "abcd1", "안녕하세요");
 
-        InputWord inputWord = new InputWord("xxxrxr", words);
-        Answer answer = new Answer("cherry");
-        MatchResults matchResults = inputWord.match(answer);
-
-        assertThat(matchResults.getResults()).containsExactly(
-                new MatchResult('x', Hint.NOT_EXIST),
-                new MatchResult('x', Hint.NOT_EXIST),
-                new MatchResult('x', Hint.NOT_EXIST),
-                new MatchResult('r', Hint.CORRECT),
-                new MatchResult('x', Hint.NOT_EXIST),
-                new MatchResult('r', Hint.EXIST)
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class,() -> Word.createInput("abcd1", words)),
+                () -> assertThrows(IllegalArgumentException.class,() -> Word.createInput("안녕하세요", words))
         );
     }
+
+
 }
