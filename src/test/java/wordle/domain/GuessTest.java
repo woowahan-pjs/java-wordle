3ec118d426034a7b9700b9134d4c9bf0f67a457b
woozi <kwj1270@naver.com>
2024-06-17 20:21:59 +0900

    test: 알파벳과 인덱스가 들어오면 해당 인덱스 이전의 알파벳 개수를 반환한다

--- src/test/java/wordle/domain/GuessTest.java
+++ src/test/java/wordle/domain/GuessTest.java
@@ -2,6 +2,7 @@ package wordle.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 public class GuessTest {
@@ -9,5 +10,13 @@ public class GuessTest {
     @Test
     void 답안은_5글자가_아니면_예외를_발생한다() {
         assertThrowsExactly(IllegalArgumentException.class, () -> new Guess("abcdef"));
+    }
+
+    @Test
+    void 알파벳과_인덱스가_들어오면_해당_인덱스_이전의_알파벳_개수를_반환한다() {
+        final Guess guess = new Guess("tasty");
+        final long count = guess.countAlphabets(Alphabet.t, 4);
+
+        assertEquals(2, count);
     }
 }
