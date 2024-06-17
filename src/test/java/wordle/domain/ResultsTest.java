d32d773cb97bf8c896e3b193d2f7a33a466ee5fc
woozi <kwj1270@naver.com>
2024-06-17 20:53:44 +0900

    test: 결과들을 가지고 있다면 개수를 반환할 수 있다

--- src/test/java/wordle/domain/ResultsTest.java
+++ src/test/java/wordle/domain/ResultsTest.java
@@ -4,10 +4,28 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ResultsTest {
+
+    @Test
+    void 결과들을_가지고_있다면_개수를_반환할_수_있다() {
+        // given
+        List<Result> resultList = List.of(
+                new Result(List.of(ResultType.EXIST, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.EXIST, ResultType.MISMATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED))
+        );
+        Results results = new Results(resultList);
+
+        // when
+        final int actual = results.size();
+
+        // then
+        assertEquals(actual, 4);
+    }
+
 
     @Test
     void 문자가_모두_일치하는_결과가_있는_경우_true를_반환한다() {
