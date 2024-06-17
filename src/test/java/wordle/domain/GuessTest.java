9d0061575fb9e90654ae75752d0589e7c934733a
woozi <kwj1270@naver.com>
2024-06-17 20:17:46 +0900

    test: 답안은 5글자가 아니면 예외를 발생한다

--- src/test/java/wordle/domain/GuessTest.java
+++ src/test/java/wordle/domain/GuessTest.java
@@ -1,6 +1,14 @@
 package wordle.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class GuessTest {
 
+    @Test
+    void 답안은_5글자가_아니면_예외를_발생한다() {
+        assertThrows(RuntimeException.class, () -> new Guess("abcdef"));
+    }
 }
 
