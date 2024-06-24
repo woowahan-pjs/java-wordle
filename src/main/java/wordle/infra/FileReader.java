2cb3d4962c76ac5a7bf3f105bcb0c7691e3ac9f9
Jongmin Yoon <jongmin4943@gmail.com>
2024-06-24 21:54:08 +0900

    fix(FileReader): try-catch-resource 구문으로 변경

--- src/main/java/wordle/infra/FileReader.java
+++ src/main/java/wordle/infra/FileReader.java
@@ -6,25 +6,30 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Stream;
+
 import wordle.exception.FileReadFailException;
 
 public class FileReader {
 
-    public List<String> readByLine(String filePath) {
+    public List<String> readByLine(final String filePath) {
         if (filePath == null || filePath.isBlank()) {
             throw new FileReadFailException();
         }
 
-        try {
-            URL systemResource = getUrl(filePath);
-            return Files.lines(Path.of(systemResource.toURI())).toList();
-        } catch (IOException | URISyntaxException e) {
+        try (final Stream<String> lines = Files.lines(getPath(filePath))) {
+            return lines.toList();
+        } catch (final IOException | URISyntaxException e) {
             throw new FileReadFailException();
         }
     }
 
-    private URL getUrl(String filePath) {
-        URL systemResource = ClassLoader.getSystemResource(filePath);
+    private Path getPath(final String filePath) throws URISyntaxException {
+        return Path.of(getUrl(filePath).toURI());
+    }
+
+    private URL getUrl(final String filePath) {
+        final URL systemResource = ClassLoader.getSystemResource(filePath);
         if (systemResource == null) {
             throw new FileReadFailException();
         }
