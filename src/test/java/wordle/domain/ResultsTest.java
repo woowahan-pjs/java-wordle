1efe596e051b7fee399c45db197f7c84edf35b99
woozi <kwj1270@naver.com>
2024-06-26 02:44:15 +0900

    test: Results 클래스 단위 테스트 소스 보강

--- src/test/java/wordle/domain/ResultsTest.java
+++ src/test/java/wordle/domain/ResultsTest.java
@@ -1,5 +1,6 @@
 package wordle.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,54 +9,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ResultsTest {
 
+    @DisplayName("결과 목록은 가지고 있는 결과의 개수를 반환할 수 있다")
     @Test
-    void 결과들을_가지고_있다면_개수를_반환할_수_있다() {
-        // given
+    void 결과_목록은_가지고_있는_결과의_개수를_반환할_수_있다() {
         final List<Result> resultList = List.of(
-                new Result(List.of(ResultType.EXIST, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
-                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED)),
-                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.EXIST, ResultType.MISMATCHED, ResultType.MATCHED)),
-                new Result(List.of(ResultType.MISMATCHED, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED))
-        );
-        final Results results = new Results(resultList);
-
-        // when
-        final int actual = results.size();
-
-        // then
-        assertEquals(actual, 4);
-    }
-
-
-    @Test
-    void 문자가_모두_일치하는_결과가_있는_경우_true를_반환한다() {
-        // given
-        final List<Result> resultList = List.of(
-                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.EXIST, ResultType.EXIST, ResultType.EXIST, ResultType.EXIST, ResultType.EXIST)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED)),
+                new Result(List.of(ResultType.EXIST, ResultType.MISMATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED)),
                 new Result(List.of(ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED))
         );
         final Results results = new Results(resultList);
 
-        // when
-        results.hasAnswer();
-
-        // then
-        assertTrue(results.hasAnswer());
+        assertEquals(resultList.size(), results.size());
     }
 
+
+    @DisplayName("결과 목록은 모든 결과가 매칭된 결과가 있는 경우 true를 반환한다")
     @Test
-    void 문자가_모두_일치하는_결과가_없는_경우_false를_반환한다() {
-        // given
+    void 결과_목록은_모든_결과가_매칭된_결과가_있는_경우_true를_반환한다() {
         final List<Result> resultList = List.of(
-                new Result(List.of(ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
-                new Result(List.of(ResultType.MATCHED, ResultType.MISMATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED))
+                new Result(List.of(ResultType.EXIST, ResultType.EXIST, ResultType.EXIST, ResultType.EXIST, ResultType.EXIST)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED)),
+                new Result(List.of(ResultType.EXIST, ResultType.MISMATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED))
         );
         final Results results = new Results(resultList);
 
-        // when
-        results.hasAnswer();
+        assertTrue(results.hasAnswer());
+    }
 
-        // then
+    @DisplayName("결과 목록은 모든 결과가 매칭된 결과가 없는 경우 false를 반환한다")
+    @Test
+    void 결과_목록은_모든_결과가_매칭된_결과가_없는_경우_false를_반환한다() {
+        final List<Result> resultList = List.of(
+                new Result(List.of(ResultType.EXIST, ResultType.EXIST, ResultType.EXIST, ResultType.EXIST, ResultType.EXIST)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED, ResultType.MISMATCHED)),
+                new Result(List.of(ResultType.EXIST, ResultType.MISMATCHED, ResultType.MATCHED, ResultType.MATCHED, ResultType.MATCHED)),
+                new Result(List.of(ResultType.MISMATCHED, ResultType.MATCHED, ResultType.EXIST, ResultType.MATCHED, ResultType.MATCHED))
+        );
+        final Results results = new Results(resultList);
+
         assertFalse(results.hasAnswer());
     }
 }
