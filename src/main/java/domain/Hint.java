21c735dbf2e36df65bd69501ac1d0aac2295f9de
jylim <jylim@12cm.co.kr>
2024-06-28 10:17:33 +0900

    chore: 오타 수정

--- src/main/java/domain/Hint.java
+++ src/main/java/domain/Hint.java
@@ -2,14 +2,14 @@ package domain;
 
 public enum Hint {
 
-    CORRECT("🟩"),           //\uD83D\\uDFE9
-    EXIST("🟨"),              //\uD83D\\uDFE8
+    CORRECT("🟩"),           // \uD83D\uDFE9
+    EXIST("🟨"),             // \uD83D\uDFE8
     NOT_EXIST("⬜");
 
     private final String tile;
 
-    Hint(String hint) {
-        this.tile = hint;
+    Hint(String tile) {
+        this.tile = tile;
     }
 
     public String getTile() {
